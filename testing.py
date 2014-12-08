453491216262ca4ff52bf9466d682a2b84b15ff6
Leonard Richardson <leonardr@segfault.org>
2014-12-08 15:11:36 -0500

    Trying to squeeze as much as possible out of tests/ so it can be reused.

--- testing.py
+++ testing.py
@@ -1,14 +1,11 @@
-import os
 from datetime import datetime
 from nose.tools import set_trace
 from sqlalchemy.orm.session import Session
 from model import (
-    Base,
     Contributor,
     CoverageRecord,
     DataSource,
     Genre,
-    SessionManager,
     LicensePool,
     Patron,
     Resource,
@@ -19,43 +16,13 @@ from model import (
 )
 from classifier import Classifier
 
-class DBInfo(object):
-    connection = None
-    engine = None
-    transaction = None
-
-def setup():
-    # Connect to the database and create the schema within a transaction
-    engine, connection = SessionManager.initialize(os.environ['DATABASE_URL_TEST'])
-    Base.metadata.drop_all(connection)
-    Base.metadata.create_all(connection)
-    DBInfo.engine = engine
-    DBInfo.connection = connection
-    DBInfo.transaction = connection.begin_nested()
-
-    db = Session(DBInfo.connection)
-    SessionManager.initialize_data(db)
-
-    # Test data: Create the patron used by the dummy authentication
-    # mechanism.
-    get_one_or_create(db, Patron, authorization_identifier="200",
-                      create_method_kwargs=dict(external_identifier="200200200"))
-    db.commit()
-
-    print "Connection is now %r" % DBInfo.connection
-    print "Transaction is now %r" % DBInfo.transaction
-
-def teardown():
-    # Roll back the top level transaction and disconnect from the database
-    DBInfo.transaction.rollback()
-    DBInfo.connection.close()
-    DBInfo.engine.dispose()
-
-
 class DatabaseTest(object):
+
+    DBInfo = None
+
     def setup(self):
-        self.__transaction = DBInfo.connection.begin_nested()
-        self._db = Session(DBInfo.connection)
+        self.__transaction = self.DBInfo.connection.begin_nested()
+        self._db = Session(self.DBInfo.connection)
         self.counter = 0
 
     def teardown(self):
@@ -181,4 +148,5 @@ class DatabaseTest(object):
                 source, pool, "application/epub+zip")
 
         return pool
+
 
