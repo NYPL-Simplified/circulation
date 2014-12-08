50ae935c59c7504ca9fada939ed1fd66d26953b1
Leonard Richardson <leonardr@segfault.org>
2014-12-08 15:14:08 -0500

    Okay, squeezed it down pretty far.

--- testing.py
+++ testing.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from nose.tools import set_trace
 from sqlalchemy.orm.session import Session
 from model import (
+    Base,
     Contributor,
     CoverageRecord,
     DataSource,
@@ -150,3 +151,39 @@ class DatabaseTest(object):
         return pool
 
 
+from nose.tools import set_trace
+import os
+from sqlalchemy.orm.session import Session
+
+from model import (
+       Patron,
+    SessionManager,
+    get_one_or_create,
+)
+
+def _setup(dbinfo):
+    # Connect to the database and create the schema within a transaction
+    engine, connection = SessionManager.initialize(os.environ['DATABASE_URL_TEST'])
+    Base.metadata.drop_all(connection)
+    Base.metadata.create_all(connection)
+    dbinfo.engine = engine
+    dbinfo.connection = connection
+    dbinfo.transaction = connection.begin_nested()
+
+    db = Session(dbinfo.connection)
+    SessionManager.initialize_data(db)
+
+    # Test data: Create the patron used by the dummy authentication
+    # mechanism.
+    get_one_or_create(db, Patron, authorization_identifier="200",
+                      create_method_kwargs=dict(external_identifier="200200200"))
+    db.commit()
+
+    print "Connection is now %r" % dbinfo.connection
+    print "Transaction is now %r" % dbinfo.transaction
+
+def _teardown(dbinfo):
+    # Roll back the top level transaction and disconnect from the database
+    dbinfo.transaction.rollback()
+    dbinfo.connection.close()
+    dbinfo.engine.dispose()
