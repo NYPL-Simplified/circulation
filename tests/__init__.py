d8a05a43d641c34f3aeeb0d667f8ac5b639856e7
Leonard Richardson <leonardr@segfault.org>
2014-12-10 12:07:00 -0500

    Pure OPDS tests should not be in the circulation manager, but putting them here for now keeps them working.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,15 +1,8 @@
 import sys, os
 from nose.tools import set_trace
 
-# Add the parent directory to the path so that import statements will work
-# the same in tests as in code.
-# this_dir = os.path.abspath(os.path.dirname(__file__))
-# parent = os.path.split(this_dir)[0]
-# sys.path.insert(0, parent)
-
 from ..core.testing import (
     DatabaseTest,
-#    DBInfo,
     _setup,
     _teardown,
 )
