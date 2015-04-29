a6804519e58e68795b373b9142353f815f4efd1a
Leonard Richardson <leonardr@segfault.org>
2015-04-29 13:18:19 +0000

    Added scripts for making things presentation-ready.

---
+++ bin/make_identifiers_without_edition_presentation_ready.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""Make books presentation ready by asking the metadata wrangler about them."""
+import os
+import sys
+bin_dir = os.path.split(__file__)[0]
+package_dir = os.path.join(bin_dir, "..")
+sys.path.append(os.path.abspath(package_dir))
+from monitor import (
+    LicensePoolButNoEditionPresentationReadyMonitor,
+)
+from core.scripts import RunMonitorScript
+RunMonitorScript(LicensePoolButNoEditionPresentationReadyMonitor).run()
