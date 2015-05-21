daea61cddaceb589433dd90cdf3f272414e153d7
Leonard Richardson <leonardr@segfault.org>
2015-05-21 09:36:56 -0400

    Properly CDNify S3 URLs that include the bucket name.

--- util/cdn.py
+++ util/cdn.py
@@ -1,10 +1,22 @@
 """Turn local URLs into CDN URLs."""
 
+from nose.tools import set_trace
 import urlparse
+from .. import s3
 
 def cdnify(url, cdn_host):
     if not cdn_host:
         return url
     scheme, netloc, path, query, fragment = urlparse.urlsplit(url)
-    scheme, netloc, i1, i2, i3 = urlparse.urlsplit(cdn_host)
-    return urlparse.urlunsplit((scheme, netloc, path, query, fragment))
+    cdn_scheme, cdn_netloc, i1, i2, i3 = urlparse.urlsplit(cdn_host)
+
+    if netloc == 's3.amazonaws.com':
+        # This is a URL like "http://s3.amazonaws.com/bucket/foo".
+        # It's equivalent to "http://bucket/foo".
+        # It should be CDNified to "http://cdn/foo".
+        #
+        # i.e. eliminate the bucket name.
+        bucket, path = s3.S3Uploader.bucket_and_filename(
+            url)
+
+    return urlparse.urlunsplit((cdn_scheme, cdn_netloc, path, query, fragment))
