ddab010f5b2a935ccb9cf64dbe1aa431ca0d566b
Leonard Richardson <leonardr@segfault.org>
2015-06-24 09:24:14 -0400

    Split out most of the problem() code into problem_raw(), which returns a 3-tuple instead of a Response object.

--- util/flask_util.py
+++ util/flask_util.py
@@ -7,11 +7,16 @@ from . import (
     languages_from_accept
 )
 
-def problem(type, title, status, detail=None, instance=None, headers={}):
-    """Create a Response that includes a Problem Detail Document."""
+def problem_raw(type, title, status, detail=None, instance=None, headers={}):
     data = problem_detail.json(type, title, status, detail, instance)
     final_headers = { "Content-Type" : problem_detail.JSON_MEDIA_TYPE }
     final_headers.update(headers)
+    return status, final_headers, data
+
+def problem(type, title, status, detail=None, instance=None, headers={}):
+    """Create a Response that includes a Problem Detail Document."""
+    status, headers, data = problem_raw(
+        title, status, detail, instance, headers)
     return Response(data, status, headers)
     
 def languages_for_request():
