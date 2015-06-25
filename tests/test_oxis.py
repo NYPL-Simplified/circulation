ca1853e2ede4bcc43c1cebcd65b32af6b3abb620
Leonard Richardson <leonardr@segfault.org>
2015-06-25 14:16:42 -0400

    Slowly inching forward the Axis parsing test.

--- tests/test_oxis.py
+++ tests/test_oxis.py
@@ -13,3 +13,10 @@ class TestParsers(object):
         data = pkgutil.get_data("tests", "files/axis/tiny_collection.xml")
 
         [bib1, av1], [bib2, av2] = BibliographicParser().process_all(data)
+
+        # If we turn off the parsing of one of these pieces of info,
+        # that piece is not parsed.
+        eq_([(None, None), (None, None)],
+            list(BibliographicParser(False, False).process_all(data)))
+
+        set_trace()
