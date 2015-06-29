3c47bf0281f362752a3efcfacded9c081a1dc07d
Leonard Richardson <leonardr@segfault.org>
2015-06-29 15:30:51 -0400

    Implemented and tested request handling for signing and accountinfo.

--- adobe_vendor_id.py
+++ adobe_vendor_id.py
@@ -1,6 +1,7 @@
 from nose.tools import set_trace
 import uuid
 import base64
+import os
 
 import flask
 from flask import Response
@@ -31,7 +32,7 @@ class AdobeVendorIDHandler(object):
             vendor_id=self.vendor_id, message=message)
 
 
-class AdobeVendorIDUtility(object):
+class AdobeVendorIDRequestHandler(object):
 
     """Standalone class that can be tested without bringing in Flask or
     the database schema.
@@ -51,16 +52,66 @@ class AdobeVendorIDUtility(object):
 
     ERROR_RESPONSE_TEMPLATE = '<error xmlns="http://ns.adobe.com/adept" data="E_%(vendor_id)s_%(type)s %(message)s"/>'
 
+    TOKEN_FAILURE = 'Incorrect token.'
+    AUTHENTICATION_FAILURE = 'Incorrect barcode or PIN.'
+    URN_LOOKUP_FAILURE = 'Could not identify patron.'
+
+    def __init__(self, node_value=None, vendor_id=None):
+        node_value = (
+            node_value or os.environ.get('ADOBE_VENDOR_ID_NODE_VALUE'))
+        self.node_value = int(node_value)
+        self.vendor_id = vendor_id or os.environ.get('ADOBE_VENDOR_ID')
+
     def uuid(self):
-        uuid = str(uuid.uuid1(self.node_value))
+        """Create a new UUID URN compatible with the Vendor ID system."""
+        u = str(uuid.uuid1(self.node_value))
         # This chop is required by the spec. I have no idea why, but
         # since the first part of the UUID is the least significant,
         # it doesn't do much damage.
-        return "urn:uuid:0" + uuid[:-1]
+        return "urn:uuid:0" + u[:-1]
 
     def error_document(self, type, message):
-        return self.ERORR_RESPONSE_TEMPLATE % dict(
+        return self.ERROR_RESPONSE_TEMPLATE % dict(
             vendor_id=self.vendor_id, type=type, message=message)
+
+    def handle_signin_request(self, data, standard_lookup, authdata_lookup):
+        parser = AdobeSignInRequestParser()
+        try:
+            data = parser.process(data)
+        except Exception, e:
+            return self.error_document(self.AUTH_ERROR_TYPE, str(e))
+        user_id = label = None
+        if data['method'] == parser.STANDARD:
+            username = data['username']
+            password = data['password']
+            user_id, label = standard_lookup(username, password)
+            failure = self.AUTHENTICATION_FAILURE
+        elif data['method'] == parser.AUTH_DATA:
+            authdata = data[parser.AUTH_DATA]
+            user_id, label = authdata_lookup(authdata)
+            failure = self.TOKEN_FAILURE
+        if user_id is None:
+            return self.error_document(self.AUTH_ERROR_TYPE, failure)
+        else:
+            return self.SIGN_IN_RESPONSE_TEMPLATE % dict(
+                user=user_id, label=label)
+
+    def handle_accountinfo_request(self, data, urn_to_label):
+        parser = AdobeAccountInfoRequestParser()
+        label = None
+        try:
+            data = parser.process(data)
+            label = urn_to_label(data['user'])
+        except Exception, e:
+            return self.error_document(
+                self.ACCOUNT_INFO_ERROR_TYPE, str(e))
+
+        if label:
+            return self.ACCOUNT_INFO_RESPONSE_TEMPLATE % dict(label=label)
+        else:
+            return self.error_document(
+                self.ACCOUNT_INFO_ERROR_TYPE, self.URN_LOOKUP_FAILURE)        
+
 
 class AdobeRequestParser(XMLParser):
 
@@ -87,18 +138,22 @@ class AdobeSignInRequestParser(AdobeRequestParser):
 
     REQUEST_XPATH = "/adept:signInRequest"
 
+    STANDARD = 'standard'
+    AUTH_DATA = 'authData'
+
     def process_one(self, tag, namespaces):
         method = tag.attrib.get('method')
 
         if not method:
             raise ValueError("No signin method specified")
         data = dict(method=method)
-        if method == 'standard':
+        if method == self.STANDARD:
             self._add(data, tag, 'username', namespaces)
             self._add(data, tag, 'password', namespaces)
-
-        elif method == 'authData':
-            self._add(data, tag, 'authData', namespaces, base64.b64decode)
+        elif method == self.AUTH_DATA:
+            self._add(data, tag, self.AUTH_DATA, namespaces, base64.b64decode)
+        else:
+            raise ValueError("Unknown signin method: %s" % method)
         return data
 
 class AdobeAccountInfoRequestParser(AdobeRequestParser):
--- tests/test_adobe_vendor_id.py
+++ tests/test_adobe_vendor_id.py
@@ -1,8 +1,11 @@
+import base64
 from nose.tools import set_trace, eq_
+import re
 
 from ..adobe_vendor_id import (
     AdobeSignInRequestParser,
     AdobeAccountInfoRequestParser,
+    AdobeVendorIDRequestHandler,
 )
 
 
@@ -39,3 +42,114 @@ class TestVendorIDRequestParsers(object):
         eq_({'method': 'standard', 
              'user': 'urn:uuid:0xxxxxxx-xxxx-1xxx-xxxx-yyyyyyyyyyyy'},
             data)
+
+class TestVendorIDRequestHandler(object):
+
+    username_sign_in_request = """<signInRequest method="standard" xmlns="http://ns.adobe.com/adept">
+<username>%(username)s</username>
+<password>%(password)s</password>
+</signInRequest>"""
+
+    authdata_sign_in_request = """<signInRequest method="authData" xmlns="http://ns.adobe.com/adept">
+<authData>%(authdata)s</authData>
+</signInRequest>"""
+
+    accountinfo_request = """<accountInfoRequest method="standard" xmlns="http://ns.adobe.com/adept">
+<user>%(uuid)s</user>
+</accountInfoRequest >"""
+
+    TEST_NODE_VALUE = 114740953091845
+    TEST_VENDOR_ID = "1045"
+
+    user1_uuid = AdobeVendorIDRequestHandler(TEST_NODE_VALUE, None).uuid()
+    user1_label = "Human-readable label for user1"
+    username_password_lookup = {
+        ("user1", "pass1") : (user1_uuid, user1_label)
+    }
+
+    authdata_lookup = {
+        "The secret token" : (user1_uuid, user1_label)
+    }
+
+    userinfo_lookup = { user1_uuid : user1_label }
+
+    @property
+    def _handler(self):
+        return AdobeVendorIDRequestHandler(
+            self.TEST_NODE_VALUE, self.TEST_VENDOR_ID)
+
+    @classmethod
+    def _standard_login(cls, username, password):
+        return cls.username_password_lookup.get(
+            (username, password), (None, None))
+
+    @classmethod
+    def _authdata_login(cls, authdata):
+        return cls.authdata_lookup.get(authdata, (None, None))
+
+    @classmethod
+    def _userinfo(cls, uuid):
+        return cls.userinfo_lookup.get(uuid)
+
+    def test_uuid(self):
+        u = self._handler.uuid()
+        # All UUIDs need to start with a 0 and end with the same node
+        # value.
+        assert u.startswith('urn:uuid:0')
+        assert u.endswith('685b35c00f0')
+
+    def test_error_document(self):
+        doc = self._handler.error_document(
+            "VENDORID", "Some random error")
+        eq_('<error xmlns="http://ns.adobe.com/adept" data="E_1045_VENDORID Some random error"/>', doc)
+
+    def test_handle_username_sign_in_request_success(self):
+        doc = self.username_sign_in_request % dict(
+            username="user1", password="pass1")
+        result = self._handler.handle_signin_request(
+            doc, self._standard_login, self._authdata_login)
+        assert result.startswith('<signInResponse xmlns="http://ns.adobe.com/adept">\n<user>urn:uuid:0')
+        assert result.endswith('-685b35c00f0</user>\n<label>Human-readable label for user1</label>\n</signInResponse>')
+
+    def test_handle_username_sign_in_request_failure(self):
+        doc = self.username_sign_in_request % dict(
+            username="user1", password="wrongpass")
+        result = self._handler.handle_signin_request(
+            doc, self._standard_login, self._authdata_login)
+        eq_('<error xmlns="http://ns.adobe.com/adept" data="E_1045_AUTH Incorrect barcode or PIN."/>', result)
+
+    def test_handle_username_authdata_request_success(self):
+        doc = self.authdata_sign_in_request % dict(
+            authdata=base64.b64encode("The secret token"))
+        result = self._handler.handle_signin_request(
+            doc, self._standard_login, self._authdata_login)
+        assert result.startswith('<signInResponse xmlns="http://ns.adobe.com/adept">\n<user>urn:uuid:0')
+        assert result.endswith('-685b35c00f0</user>\n<label>Human-readable label for user1</label>\n</signInResponse>')
+
+    def test_handle_username_authdata_request_invalid(self):
+        doc = self.authdata_sign_in_request % dict(
+            authdata="incorrect")
+        result = self._handler.handle_signin_request(
+            doc, self._standard_login, self._authdata_login)
+        assert result.startswith('<error xmlns="http://ns.adobe.com/adept" data="E_1045_AUTH')
+
+    def test_handle_username_authdata_request_failure(self):
+        doc = self.authdata_sign_in_request % dict(
+            authdata=base64.b64encode("incorrect"))
+        result = self._handler.handle_signin_request(
+            doc, self._standard_login, self._authdata_login)
+        eq_('<error xmlns="http://ns.adobe.com/adept" data="E_1045_AUTH Incorrect token."/>', result)
+
+    def test_handle_accountinfo_success(self):
+        doc = self.accountinfo_request % dict(
+            uuid=self.user1_uuid)
+        result = self._handler.handle_accountinfo_request(
+            doc, self._userinfo)
+        eq_('<accountInfoResponse xmlns="http://ns.adobe.com/adept">\n<label>Human-readable label for user1</label>\n</accountInfoResponse>', result)
+
+    def test_handle_accountinfo_failure(self):
+        doc = self.accountinfo_request % dict(
+            uuid="not the uuid")
+        result = self._handler.handle_accountinfo_request(
+            doc, self._userinfo)
+        eq_('<error xmlns="http://ns.adobe.com/adept" data="E_1045_ACCOUNT_INFO Could not identify patron."/>', result)
