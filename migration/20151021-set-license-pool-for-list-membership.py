f3c28576a5bdf5bc8fecc5bf9ef7195a5a87ab87
Leonard Richardson <leonardr@segfault.org>
2015-10-21 12:11:15 -0400

    Added migration script to set LicensePool for CustomListEntries.

---
+++ migration/20151021-set-license-pool-for-list-membership.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""Make sure every CustomListEntry has a LicensePool set.
+"""
+import os
+import sys
+import logging
+from pdb import set_trace
+bin_dir = os.path.split(__file__)[0]
+package_dir = os.path.join(bin_dir, "..", "..")
+sys.path.append(os.path.abspath(package_dir))
+
+from nose.tools import set_trace
+from core.model import (
+    production_session,
+    CustomListEntry,
+)
+
+_db = production_session()
+
+qu = _db.query(CustomListEntry).filter(CustomListEntry.license_pool==None)
+print "Fixing %d custom list entries with no licensepool." % qu.count()
+
+for cle in qu: 
+    cle.set_license_pool()
