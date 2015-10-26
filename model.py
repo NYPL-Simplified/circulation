0fb099dc8e1b76be61468057f97ac34e9ba46780
Leonard Richardson <leonardr@segfault.org>
2015-10-26 16:09:46 -0400

    Basic implementation and test of Complaint.

--- model.py
+++ model.py
@@ -5450,6 +5450,9 @@ class LicensePool(Base):
     # One LicensePool may have many associated Hyperlinks.
     links = relationship("Hyperlink", backref="license_pool")
 
+    # One LicensePool can be associated with many Complaints.
+    complaints = relationship('Complaint', backref='license_pool')
+
     # The date this LicensePool first became available.
     availability_time = Column(DateTime, index=True)
 
@@ -6795,6 +6798,76 @@ class CustomListEntry(Base):
             )
         self.license_pool = new_license_pool
         return self.license_pool
+
+
+class Complaint(Base):
+    """A complaint about a LicensePool."""
+
+    __tablename__ = 'complaints'
+
+    VALID_TYPES = set([
+        "http://librarysimplified.org/terms/problem/" + x
+        for x in [
+                'wrong-genre',
+                'wrong-audience', 
+                'wrong-age-range',
+                'cannot-fulfill-loan', 
+                'cannot-issue-loan',
+                'cannot-render',
+              ]
+    ])
+
+    id = Column(Integer, primary_key=True)
+
+    # One LicensePool can have many complaints lodged against it.
+    license_pool_id = Column(
+        Integer, ForeignKey('licensepools.id'), index=True)
+
+    # The type of complaint.
+    type = Column(String, nullable=False, index=True) 
+
+    # The source of the complaint.
+    source = Column(String, nullable=True, index=True)
+
+    # Detailed information about the complaint.
+    detail = Column(String, nullable=True)
+
+    timestamp = Column(DateTime, nullable=False)
+
+    @classmethod
+    def register(self, license_pool, type, source, detail):
+        """Register a problem detail document as a Complaint against the
+        given LicensePool.
+        """
+        if not license_pool:
+            raise ValueError("No license pool provided")
+        _db = Session.object_session(license_pool)
+        if type not in self.VALID_TYPES:
+            raise ValueError("Unrecognized complaint type: %s" % type)
+        now = datetime.datetime.utcnow()
+        if source:
+            complaint, is_new = get_one_or_create(
+                _db, Complaint,
+                license_pool=license_pool, 
+                source=source, type=type,
+                on_multiple='interchangeable',
+                create_method_kwargs = dict(
+                    timestamp=now,
+                )
+            )
+            complaint.timestamp = now
+            complaint.detail = detail
+        else:
+            complaint, is_new = create(
+                _db,
+                Complaint,
+                license_pool=license_pool, 
+                source=source, 
+                type=type,
+                timestamp=now,
+                detail=detail
+            )
+        return complaint, is_new
 
 from sqlalchemy.sql import compiler
 from psycopg2.extensions import adapt as sqlescape
--- tests/test_model.py
+++ tests/test_model.py
@@ -24,6 +24,7 @@ from config import (
 
 from model import (
     CirculationEvent,
+    Complaint,
     Contributor,
     CoverageRecord,
     Credential,
@@ -2274,3 +2275,62 @@ class TestPatron(DatabaseTest):
             config[Configuration.POLICIES][key] = "(not a valid regexp"
             assert_raises(TypeError, lambda x: patron.external_type)
             patron._external_type = None
+
+
+class TestComplaint(DatabaseTest):
+
+    def setup(self):
+        super(TestComplaint, self).setup()
+        self.edition, self.pool = self._edition(with_license_pool=True)
+        self.type = "http://librarysimplified.org/terms/problem/wrong-genre"
+
+    def test_success(self):
+        complaint, is_new = Complaint.register(
+            self.pool, self.type, "foo", "bar"
+        )
+        eq_(True, is_new)
+        eq_(self.type, complaint.type)
+        eq_("foo", complaint.source)
+        eq_("bar", complaint.detail)
+        assert abs(datetime.datetime.utcnow() -complaint.timestamp).seconds < 3
+
+        # A second complaint from the same source is folded into the
+        # original complaint.
+        complaint2, is_new = Complaint.register(
+            self.pool, self.type, "foo", "baz"
+        )
+        eq_(False, is_new)
+        eq_(complaint.id, complaint2.id)
+        eq_("baz", complaint.detail)
+
+        eq_(1, len(self.pool.complaints))
+
+    def test_success_no_source(self):
+        complaint, is_new = Complaint.register(
+            self.pool, self.type, None, None
+        )
+        eq_(True, is_new)
+        eq_(self.type, complaint.type)
+        eq_(None, complaint.source)
+
+        # A second identical complaint from no source is treated as a
+        # separate complaint.
+        complaint2, is_new = Complaint.register(
+            self.pool, self.type, None, None
+        )
+        eq_(True, is_new)
+        eq_(None, complaint.source)
+        assert complaint2.id != complaint.id
+
+        eq_(2, len(self.pool.complaints))
+
+    def test_failure_no_licensepool(self):
+        assert_raises(
+            ValueError, Complaint.register, self.pool, type, None, None
+        )
+
+    def test_unrecognized_type(self):
+        type = "http://librarysimplified.org/terms/problem/no-such-error"
+        assert_raises(
+            ValueError, Complaint.register, self.pool, type, None, None
+        )
