9581fc1d7b593e52f5ba7eb8ae9bbda9817ceeaf
Courteney Ervin <courteneyervin@nypl.org>
2016-01-05 16:48:26 -0500

    Add verbose OPDS entries to materialized views

--- files/materialized_view_works_workgenres.sql
+++ files/materialized_view_works_workgenres.sql
@@ -28,6 +28,7 @@ as
     works.random,
     works.last_update_time,
     works.simple_opds_entry,
+    works.verbose_opds_entry,
     licensepools.id AS license_pool_id
    FROM works
      JOIN editions ON editions.work_id = works.id AND editions.is_primary_for_work = true
