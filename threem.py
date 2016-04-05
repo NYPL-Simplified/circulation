f8139a8995eeaa487773f3d9e59c891522b66ae0
Leonard Richardson <leonardr@segfault.org>
2016-04-05 11:57:05 -0400

    Reduce the weight of 3M classifications because of the whole 'juvenile' problem. Still pretty weighty though.

--- threem.py
+++ threem.py
@@ -209,7 +209,7 @@ class ItemListParser(XMLParser):
             if not i:
                 continue
             i = i.replace("&amp;amp;", "&amp;").replace("&amp;", "&").replace("&#39;", "'")
-            genres.append(SubjectData(Subject.THREEM, i, weight=30))
+            genres.append(SubjectData(Subject.THREEM, i, weight=15))
         return genres
 
     def process_one(self, tag, namespaces):
