d0349189eec28d133d5b6b464103e3301bf74d82
Leonard Richardson <leonardr@segfault.org>
2016-04-14 10:01:26 -0400

    Turn off debug.

--- classifier.py
+++ classifier.py
@@ -3356,7 +3356,7 @@ class WorkClassifier(object):
     nonfiction_publishers = set(["Wiley"])
     fiction_publishers = set([])
 
-    def __init__(self, work, test_session=None, debug=True):
+    def __init__(self, work, test_session=None, debug=False):
         self._db = Session.object_session(work)
         if test_session:
             self._db = test_session
