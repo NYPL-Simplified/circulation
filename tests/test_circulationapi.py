6c4d3df12f1241a46cd4ef634e648437914e9a36
Leonard Richardson <leonardr@segfault.org>
2016-05-20 14:43:51 -0400

    Moved the mock API to testing and got the circulation tests to pass.

--- tests/test_circulationapi.py
+++ tests/test_circulationapi.py
@@ -1,6 +1,4 @@
 """Test the CirculationAPI."""
-import logging
-from collections import defaultdict
 from nose.tools import (
     assert_raises_regexp,
     set_trace,
@@ -14,8 +12,6 @@ from datetime import (
 
 from api.circulation_exceptions import *
 from api.circulation import (
-    BaseCirculationAPI,
-    CirculationAPI,
     LoanInfo,
     HoldInfo,
 )
@@ -28,116 +24,7 @@ from core.model import (
 )
 
 from . import DatabaseTest
-
-class MockRemoteAPI(object):
-    def __init__(self, set_delivery_mechanism_at, can_revoke_hold_when_reserved):
-        self.SET_DELIVERY_MECHANISM_AT = set_delivery_mechanism_at
-        self.CAN_REVOKE_HOLD_WHEN_RESERVED = can_revoke_hold_when_reserved
-        self.responses = defaultdict(list)
-        self.log = logging.getLogger("Mock remote API")
-
-    def checkout(
-            self, patron_obj, patron_password, licensepool, 
-            delivery_mechanism
-    ):
-        # Should be a LoanInfo.
-        return self._return_or_raise('checkout')
-                
-    def place_hold(self, patron, pin, licensepool, 
-                   hold_notification_email=None):
-        # Should be a HoldInfo.
-        return self._return_or_raise('hold')
-
-    def fulfill(self, patron, password, pool, delivery_mechanism):
-        # Should be a FulfillmentInfo.
-        return self._return_or_raise('fulfill')
-
-    def checkin(self, patron, pin, licensepool):
-        # Return value is not checked.
-        return self._return_or_raise('checkin')
-
-    def release_hold(self, patron, pin, licensepool):
-        # Return value is not checked.
-        return self.return_or_raise('release_hold')
-
-    def internal_format(self, delivery_mechanism):
-        return delivery_mechanism
-
-    def queue_checkout(self, response):
-        self._queue('checkout', response)
-
-    def queue_hold(self, response):
-        self._queue('hold', response)
-
-    def queue_fulfill(self, response):
-        self._queue('fulfill', response)
-
-    def queue_checkin(self, response):
-        self._queue('checkin', response)
-
-    def queue_release_hold(self, response):
-        self._queue('release_hold', response)
-
-    def _queue(self, k, v):
-        self.responses[k].append(v)
-
-    def _return_or_raise(self, k):
-        self.log.debug(k)
-        l = self.responses[k]
-        v = l.pop()
-        if isinstance(v, Exception):
-            raise v
-        return v
-
-class MockCirculationAPI(CirculationAPI):
-
-    def __init__(self, _db):
-        super(MockCirculationAPI, self).__init__(_db)
-        self.responses = defaultdict(list)
-        self.remote_loans = []
-        self.remote_holds = []
-        self.identifier_type_to_data_source_name = {
-            Identifier.GUTENBERG_ID: DataSource.GUTENBERG,
-            Identifier.OVERDRIVE_ID: DataSource.OVERDRIVE,
-            Identifier.THREEM_ID: DataSource.THREEM,
-            Identifier.AXIS_360_ID: DataSource.AXIS_360,
-        }
-        self.data_source_ids_for_sync = [
-            DataSource.lookup(self._db, name).id for name in 
-            self.identifier_type_to_data_source_name.values()
-        ]
-        self.remotes = {}
-
-    def local_loans(self, patron):
-        return self._db.query(Loan).filter(Loan.patron==patron)
-
-    def local_holds(self, patron):
-        return self._db.query(Hold).filter(Loan.patron==patron)
-
-    def add_remote_loan(self, *args, **kwargs):
-        self.remote_loans.append(LoanInfo(*args, **kwargs))
-
-    def add_remote_hold(self, *args, **kwargs):
-        self.remote_loans.append(LoanInfo(*args, **kwargs))
-
-    def patron_activity(self, patron, pin):
-        """Return a 2-tuple (loans, holds)."""
-        return self.remote_loans, self.remote_holds
-
-    def api_for_license_pool(self, licensepool):
-        source = licensepool.data_source.name
-        if source not in self.remotes:
-            set_delivery_mechanism_at = BaseCirculationAPI.FULFILL_STEP
-            can_revoke_hold_when_reserved = True
-            if source == DataSource.AXIS_360:
-                set_delivery_mechanism_at = BaseCirculationAPI.BORROW_STEP
-            if source == DataSource.THREEM:
-                can_revoke_hold_when_reserved = False
-            remote = MockRemoteAPI(
-                set_delivery_mechanism_at, can_revoke_hold_when_reserved
-            )
-            self.remotes[source] = remote
-        return self.remotes[source]
+from testing import MockCirculationAPI
 
 
 class TestCirculationAPI(DatabaseTest):
