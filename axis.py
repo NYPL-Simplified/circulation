402f0532acc28ad1fef448569452e9f44735a0ec
Matthew Skomarovsky <matthewskomarovsky@nypl.org>
2016-05-24 13:04:31 -0400

    Metadata.apply() clears fields with 'none' values

--- axis.py
+++ axis.py
@@ -63,7 +63,6 @@ class Axis360API(object):
             self.base_url = self.QA_BASE_URL
         elif self.base_url == 'production':
             self.base_url = self.PRODUCTION_BASE_URL
-        print self.base_url
         self.token = None
 
     @classmethod
