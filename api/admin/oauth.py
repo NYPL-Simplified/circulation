1b12617635f7bb1e011b2f68a5de884a6f374d9f
Matthew Skomarovsky <matthewskomarovsky@nypl.org>
2016-06-01 11:59:24 -0400

    copy config before adding keys

--- api/admin/oauth.py
+++ api/admin/oauth.py
@@ -22,9 +22,9 @@ class GoogleAuthService(object):
     def from_environment(cls, redirect_uri, test_mode=False):
         if test_mode:
             return cls('/path', '/callback', test_mode)
-        config = Configuration.integration(
+        config = dict(Configuration.integration(
             Configuration.GOOGLE_OAUTH_INTEGRATION
-        )['web']
+        )['web'])
         return cls(config, redirect_uri, test_mode)
 
     def callback(self, request={}):
