1798b5e84cbb333c215a81a046832bea138b5b1c
Amy Slagle <amyslagle@nypl.org>
2016-06-28 14:26:48 -0400

    Added test for clever patron external types.

--- tests/clever/test_clever.py
+++ tests/clever/test_clever.py
@@ -7,6 +7,10 @@ from api.clever import (
     UNSUPPORTED_CLEVER_USER_TYPE,
     CLEVER_NOT_ELIGIBLE,
 )
+from core.model import (
+    Patron,
+    get_one,
+)
 from .. import DatabaseTest
 
 class MockAPI(CleverAuthenticationAPI):
@@ -62,8 +66,58 @@ class TestCleverAuthenticationAPI(DatabaseTest):
         eq_('token', token)
         eq_('Abcd', patron_info.get('name'))
 
+        # A patron was created
+        patron = get_one(self._db, Patron, authorization_identifier='1234')
+        assert patron != None
+
     def test_oauth_callback_free_lunch_status(self):
         pass
 
     def test_oauth_callback_external_type(self):
-        pass
+        # Teacher is all-access
+        self.api.queue_response(dict(data=dict(name='#DEMO Open eBooks Sandbox District')))
+        self.api.queue_response(dict(data=dict(location=dict(state='TN'))))
+        self.api.queue_response(dict(data=dict(school='1234', district='1234', name='Abcd')))
+        self.api.queue_response(dict(type='teacher', data=dict(id='1'), links=[dict(rel='canonical', uri='test')]))
+        self.api.queue_response(dict(access_token='token'))
+
+        self.api.oauth_callback(self._db, {})
+
+        patron = get_one(self._db, Patron, authorization_identifier='1')
+        eq_('A', patron.external_type)
+
+        # Student type is based on grade
+
+        self.api.queue_response(dict(data=dict(name='#DEMO Open eBooks Sandbox District')))
+        self.api.queue_response(dict(data=dict(location=dict(state='TN'))))
+        self.api.queue_response(dict(data=dict(school='1234', district='1234', name='Abcd', grade='1')))
+        self.api.queue_response(dict(type='student', data=dict(id='2'), links=[dict(rel='canonical', uri='test')]))
+        self.api.queue_response(dict(access_token='token'))
+
+        self.api.oauth_callback(self._db, {})
+
+        patron = get_one(self._db, Patron, authorization_identifier='2')
+        eq_('E', patron.external_type)
+
+        self.api.queue_response(dict(data=dict(name='#DEMO Open eBooks Sandbox District')))
+        self.api.queue_response(dict(data=dict(location=dict(state='TN'))))
+        self.api.queue_response(dict(data=dict(school='1234', district='1234', name='Abcd', grade='6')))
+        self.api.queue_response(dict(type='student', data=dict(id='3'), links=[dict(rel='canonical', uri='test')]))
+        self.api.queue_response(dict(access_token='token'))
+
+        self.api.oauth_callback(self._db, {})
+
+        patron = get_one(self._db, Patron, authorization_identifier='3')
+        eq_('M', patron.external_type)
+
+        self.api.queue_response(dict(data=dict(name='#DEMO Open eBooks Sandbox District')))
+        self.api.queue_response(dict(data=dict(location=dict(state='TN'))))
+        self.api.queue_response(dict(data=dict(school='1234', district='1234', name='Abcd', grade='9')))
+        self.api.queue_response(dict(type='student', data=dict(id='4'), links=[dict(rel='canonical', uri='test')]))
+        self.api.queue_response(dict(access_token='token'))
+
+        self.api.oauth_callback(self._db, {})
+
+        patron = get_one(self._db, Patron, authorization_identifier='4')
+        eq_('H', patron.external_type)
+
