4f7abe0a14dd44809a575febe45f5bb826cf49bb
Leonard Richardson <leonardr@segfault.org>
2016-07-08 15:43:01 -0400

    Changed test to reflect the fact that exceptions are now treated as transient errors.

--- tests/test_opds_import.py
+++ tests/test_opds_import.py
@@ -673,7 +673,7 @@ class TestOPDSImporter(OPDSImporterTest):
         # There's an error message for the work that failed. 
         failure = failures['http://www.gutenberg.org/ebooks/10441']
         assert isinstance(failure, CoverageFailure)
-        eq_(False, failure.transient)
+        eq_(True, failure.transient)
         assert "Utter work failure!" in failure.exception
 
     def test_consolidate_links(self):
