deca5a29467f6d64d16b4a0f7906219883d5ad0b
Darya Chernikhova <daryachernikhova@nypl.org>
2016-11-21 13:17:06 -0500

    fixing unit tests to mock config from MockOneClick constructor.

--- tests/test_oneclick.py
+++ tests/test_oneclick.py
@@ -59,7 +59,7 @@ class OneClickAPITest(DatabaseTest):
     def setup(self):
         super(OneClickAPITest, self).setup()
 
-        self.api = MockOneClickAPI.from_config(self._db)
+        self.api = MockOneClickAPI(self._db)
         base_path = os.path.split(__file__)[0]
         self.resource_path = os.path.join(base_path, "files", "oneclick")
 
