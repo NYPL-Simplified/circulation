294c0e11d91637a6ed07b36981dbbcff9a2844c2
Leonard Richardson <leonardr@segfault.org>
2016-11-28 10:38:43 -0500

    Added docstrings.

--- api/sip/__init__.py
+++ api/sip/__init__.py
@@ -16,6 +16,30 @@ class SIP2AuthenticationProvider(BasicAuthenticationProvider):
                  login_password, location_code, field_separator,
                  client=None,
                  **kwargs):
+        """An object capable of communicating with a SIP server.
+
+        :param server: Hostname of the SIP server.
+        :param port: The port number to connect to on the SIP server.
+
+        :param login_user_id: SIP field CN; the user ID to use when
+         initiating a SIP session, if necessary. This is _not_ a
+         patron identifier (SIP field AA); it identifies the SC
+         creating the SIP session. SIP2 defines SC as "...any library
+         automation device dealing with patrons or library materials."
+
+        :param login_password: Sip field CO; the password to use when
+         initiating a SIP session, if necessary.
+
+        :param location_code: SIP field CP; the location code to use
+         when initiating a SIP session
+
+        :param field_separator: The field delimiter (see
+        "Variable-length fields" in the SIP2 spec). If no value is
+        specified, the default (the pipe character) will be used.
+
+        :param client: A drop-in replacement for the SIPClient
+        object. Only intended for use during testing.
+        """
         super(SIP2AuthenticationProvider, self).__init__(**kwargs)
         if client:
             self.client = client
@@ -32,6 +56,11 @@ class SIP2AuthenticationProvider(BasicAuthenticationProvider):
 
     @classmethod
     def info_to_patrondata(cls, info):
+
+        """Convert the SIP-specific dictionary obtained from
+        SIPClient.patron_information() to an abstract,
+        authenticator-independent PatronData object.
+        """
         if info.get('valid_patron_password') == 'N':
             # The patron did not authenticate correctly. Don't
             # return any data.
