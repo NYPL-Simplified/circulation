81b73a3df3f6d6d17bc8f2914c8c868e434d8b04
Darya Chernikhova <daryachernikhova@nypl.org>
2017-01-05 18:30:37 -0500

    Added OneClick-type books to MetadataWrangler coverage requests.

--- api/coverage.py
+++ api/coverage.py
@@ -158,7 +158,7 @@ class OPDSImportCoverageProvider(CoverageProvider):
 
     def import_feed_response(self, response, id_mapping):
         """Confirms OPDS feed response and imports feed.
-        """        
+        """
         self.check_content_type(response)
         importer = OPDSImporter(self._db, identifier_mapping=id_mapping,
                                 data_source_name=self.output_source.name)
@@ -204,6 +204,7 @@ class MetadataWranglerCoverageProvider(OPDSImportCoverageProvider):
                 Identifier.THREEM_ID,
                 Identifier.GUTENBERG_ID, 
                 Identifier.AXIS_360_ID,
+                Identifier.ONECLICK_ID, 
             ]
         output_source = DataSource.lookup(
             _db, DataSource.METADATA_WRANGLER
@@ -254,7 +255,7 @@ class MetadataWranglerCoverageProvider(OPDSImportCoverageProvider):
         """
         mapping = dict()
         for identifier in batch:
-            if identifier.type in [Identifier.AXIS_360_ID, Identifier.THREEM_ID]:
+            if identifier.type in [Identifier.AXIS_360_ID, Identifier.THREEM_ID, Identifier.ONECLICK_ID]:
                 for e in identifier.equivalencies:
                     if e.output.type == Identifier.ISBN:
                         mapping[e.output] = identifier
