59ad968f51b6135160522a32f52e2e940a020ab0
Leonard Richardson <leonardr@segfault.org>
2017-02-23 11:36:47 -0500

    While we're at it, copy library registry information from JSON into the database.

--- migration/20170224-copy-collection-configuration-into-database.py
+++ migration/20170224-copy-collection-configuration-into-database.py
@@ -7,7 +7,7 @@ import os
 import sys
 from pdb import set_trace
 bin_dir = os.path.split(__file__)[0]
-package_dir = os.path.join(bin_dir, "..")
+package_dir = os.path.join(bin_dir, u"..")
 sys.path.append(os.path.abspath(package_dir))
 from api.config import Configuration
 from core.model import (
@@ -24,11 +24,19 @@ from core.model import (
 
 _db = production_session()
 
+def copy_library_registry_information(_db, library):
+    config = Configuration.integration("Adobe Vendor ID")
+    if not config:
+        print u"No Adobe Vendor ID configuration, not setting short name or secret."
+    library.library_registry_short_name = config.get("library_short_name")
+    library.library_registry_shared_secret = config.get("authdata_secret")
+
+
 def convert_overdrive(_db, library):
     config = Configuration.integration('Overdrive')
     if not config:
-        print "No Overdrive configuration, not creating a Collection for it."
-    print "Creating Collection object for Overdrive collection."
+        print u"No Overdrive configuration, not creating a Collection for it."
+    print u"Creating Collection object for Overdrive collection."
     username = config.get('client_key')
     password = config.get('client_secret')
     library_id = config.get('library_id')
@@ -48,9 +56,9 @@ def convert_overdrive(_db, library):
 def convert_bibliotheca(_db, library):
     config = Configuration.integration('3M')
     if not config:
-        print "No Bibliotheca configuration, not creating a Collection for it."
+        print u"No Bibliotheca configuration, not creating a Collection for it."
         return
-    print "Creating Collection object for Bibliotheca collection."
+    print u"Creating Collection object for Bibliotheca collection."
     username = config.get('account_id')
     password = config.get('account_key')
     library_id = config.get('library_id')
@@ -67,13 +75,13 @@ def convert_bibliotheca(_db, library):
 def convert_axis(_db, library):
     config = Configuration.integration('Axis 360')
     if not config:
-        print "No Axis 360 configuration, not creating a Collection for it."
+        print u"No Axis 360 configuration, not creating a Collection for it."
         return
-    print "Creating Collection object for Axis 360 collection."
+    print u"Creating Collection object for Axis 360 collection."
     username = config.get('username')
     password = config.get('password')
     library_id = config.get('library_id')
-    # This is not technically a URL, it's "production" or "staging",
+    # This is not technically a URL, it's u"production" or u"staging",
     # but it's converted into a URL internally.
     url = config.get('server')
     collection, ignore = get_one_or_create(
@@ -90,7 +98,7 @@ def convert_axis(_db, library):
 def convert_content_server(_db, library):
     config = Configuration.integration("Content Server")
     if not config:
-        print "No content server configuration, not creating a Collection for it."
+        print u"No content server configuration, not creating a Collection for it."
         return
     url = config.get('url')
     collection, ignore = get_one_or_create(
@@ -102,8 +110,10 @@ def convert_content_server(_db, library):
     collection.url = url
     
 library = Library.instance(_db)
+copy_library_registry_information(_db, library)
 convert_overdrive(_db, library)
 convert_bibliotheca(_db, library)
 convert_axis(_db, library)
 convert_content_server(_db, library)
+set_trace()
 _db.commit()
