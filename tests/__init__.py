3d8bb1f21671fc4a7db68ad0082703f99b603916
Courteney Ervin <courteneyervin@nypl.org>
2017-04-04 11:59:43 -0400

    Close test files

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,4 +12,6 @@ def sample_data(filename, sample_data_dir):
     base_path = os.path.split(__file__)[0]
     resource_path = os.path.join(base_path, "files", sample_data_dir)
     path = os.path.join(resource_path, filename)
-    return open(path).read()
+
+    with open(path) as f:
+        return f.read()
