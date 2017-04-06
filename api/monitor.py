6a1d1436387f4be3b88b717029db0cb235c2048f
Leonard Richardson <leonardr@segfault.org>
2017-04-06 13:55:51 -0400

    Brought monitor.py up to date with the new code.

--- api/monitor.py
+++ api/monitor.py
@@ -18,28 +18,15 @@ from core.model import (
 from core.external_search import ExternalSearchIndex
 
 
-class UpdateOpenAccessURL(EditionSweepMonitor):
-    """Set Edition.open_access_full_url for all Gutenberg works."""
-
-    def __init__(self, _db, batch_size=100, interval_seconds=600):
-        super(UpdateOpenAccessURL, self).__init__(
-            _db, 
-            "Update open access URLs for Gutenberg editions", 
-            interval_seconds)
-        self.batch_size = batch_size
-    
-    def edition_query(self):
-        gutenberg = DataSource.lookup(self._db, DataSource.GUTENBERG)
-        return self._db.query(Edition).filter(
-            Edition.data_source==gutenberg)
-
-    def process_edition(self, edition):
-        edition.set_open_access_link()
-
 class SearchIndexMonitor(WorkSweepMonitor):
     """Make sure the search index is up-to-date for every work."""
-
-    def __init__(self, _db, index_name=None, index_client=None, batch_size=500, **kwargs):
+    SERVICE_NAME = "Search index update"
+    DEFAULT_BATCH_SIZE = 500
+    
+    def __init__(self, _db, collection, index_name=None, index_client=None,
+                 **kwargs):
+        super(SearchIndexMonitor, self).__init__(_db, collection, **kwargs)
+        
         if index_client:
             # This would only happen during a test.
             self.search_index_client = index_client
@@ -49,18 +36,22 @@ class SearchIndexMonitor(WorkSweepMonitor):
             )
 
         index_name = self.search_index_client.works_index
-        super(SearchIndexMonitor, self).__init__(
-            _db,
-            "Search index update (%s)" % index_name,
-            batch_size=batch_size,
-            **kwargs
-        )
+        # We got a generic service name. Replace it with a more
+        # specific one.
+        self.service_name = "Search index update (%s)" % index_name
 
-    def process_batch(self, batch):
-        """Update the search ndex for a set of Works."""
+    def process_batch(self, offset):
+        """Update the search index for a set of Works."""
+        batch = self.fetch_batch(offset).all()
+        if batch:
+            successes, failures = self.search_index_client.bulk_update(batch)
 
-        successes, failures = self.search_index_client.bulk_update(batch)
-
-        for work, message in failures:
-            self.log.error("Failed to update search index for %s: %s" % (work, message))
-
+            for work, message in failures:
+                self.log.error(
+                    "Failed to update search index for %s: %s", work, message
+                )
+            # Start work on the next batch.
+            return batch[-1].id
+        else:
+            # We're done.
+            return 0
