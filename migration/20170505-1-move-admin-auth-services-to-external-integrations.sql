8e563e06b2eed8008282e33294107d509f4875d5
Courteney Ervin <courteneyervin@nypl.org>
2017-05-09 13:27:09 -0400

    Avoid errors when table doesn't exist (esp. in QA/production)

--- migration/20170505-1-move-admin-auth-services-to-external-integrations.sql
+++ migration/20170505-1-move-admin-auth-services-to-external-integrations.sql
@@ -1,6 +1,1 @@
-UPDATE externalintegrations ei
-SET type = 'admin_auth', provider = aas.provider
-FROM adminauthenticationservices as aas
-WHERE ei.id = aas.external_integration_id;
-
-DROP TABLE adminauthenticationservices;
+DROP TABLE IF EXISTS adminauthenticationservices;
