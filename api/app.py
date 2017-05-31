d535f0d85ccdba63b4419cfee1dc69216da28295
Amy Slagle <amyslagle@nypl.org>
2017-05-31 16:42:01 -0400

    Only set app.config['SERVER_NAME'] when running the app.

--- api/app.py
+++ api/app.py
@@ -29,19 +29,6 @@ app.config['BABEL_DEFAULT_LOCALE'] = LanguageCodes.three_to_two[Configuration.lo
 app.config['BABEL_TRANSLATION_DIRECTORIES'] = "../translations"
 babel = Babel(app)
 
-url = Configuration.integration_url(
-    Configuration.CIRCULATION_MANAGER_INTEGRATION, required=True)
-scheme, netloc, path, parameters, query, fragment = urlparse.urlparse(url)
-if ':' in netloc:
-    host, port = netloc.split(':')
-    port = int(port)
-else:
-    host = netloc
-    port = 80
-
-# Required for subdomain support.
-app.config['SERVER_NAME'] = netloc
-
 import routes
 if Configuration.get(Configuration.INCLUDE_ADMIN_INTERFACE):
     import admin.routes
@@ -52,6 +39,19 @@ app.config['DEBUG'] = debug
 app.debug = debug
 
 def run():
+    url = Configuration.integration_url(
+        Configuration.CIRCULATION_MANAGER_INTEGRATION, required=True)
+    scheme, netloc, path, parameters, query, fragment = urlparse.urlparse(url)
+    if ':' in netloc:
+        host, port = netloc.split(':')
+        port = int(port)
+    else:
+        host = netloc
+        port = 80
+
+    # Required for subdomain support.
+    app.config['SERVER_NAME'] = netloc
+
     debug = True
 
     # Workaround for a "Resource temporarily unavailable" error when
