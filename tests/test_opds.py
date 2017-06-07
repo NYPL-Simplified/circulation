3ce5045c4d52cccc48bf96d2c5525e653e98d2e3
Leonard Richardson <leonardr@segfault.org>
2017-06-07 13:45:04 -0400

    Fixed OPDS tests relating to the caching policy.

--- tests/test_opds.py
+++ tests/test_opds.py
@@ -19,6 +19,7 @@ from config import (
 )
 from model import (
     CachedFeed,
+    ConfigurationSetting,
     Contributor,
     DataSource,
     DeliveryMechanism,
@@ -869,10 +870,11 @@ class TestOPDS(DatabaseTest):
         work1.quality = 0.75
         work2 = self._work(genre=Urban_Fantasy, with_open_access_download=True)
         work2.quality = 0.75
+
         with temp_config() as config:
             config['policies'] = {}
             config['policies'][Configuration.FEATURED_LANE_SIZE] = 2
-            config['policies'][Configuration.GROUPS_MAX_AGE_POLICY] = Configuration.CACHE_FOREVER
+
             annotator = TestAnnotatorWithGroup()
 
             # By policy, group feeds are cached forever, which means
@@ -959,7 +961,6 @@ class TestOPDS(DatabaseTest):
         with temp_config() as config:
             config['policies'] = {}
             config['policies'][Configuration.FEATURED_LANE_SIZE] = 2
-            config['policies'][Configuration.GROUPS_MAX_AGE_POLICY] = Configuration.CACHE_FOREVER
             annotator = TestAnnotator()
 
             feed = AcquisitionFeed.groups(
@@ -1056,33 +1057,33 @@ class TestOPDS(DatabaseTest):
                 pagination=Pagination.default(), use_materialized_works=False
             )
 
-        with temp_config() as config:
-            config['policies'] = {
-                Configuration.PAGE_MAX_AGE_POLICY : 10
-            }
+        af = AcquisitionFeed
+        policy = ConfigurationSetting.sitewide(
+            self._db, af.NONGROUPED_MAX_AGE_POLICY)
+        policy.value = "10"
 
-            cached1 = make_page()
-            assert work1.title in cached1.content
-            old_timestamp = cached1.timestamp
+        cached1 = make_page()
+        assert work1.title in cached1.content
+        old_timestamp = cached1.timestamp
 
-            work2 = self._work(
-                title="A Brand New Title", 
-                genre=Epic_Fantasy, with_open_access_download=True
-            )
+        work2 = self._work(
+            title="A Brand New Title", 
+            genre=Epic_Fantasy, with_open_access_download=True
+        )
 
-            # The new work does not show up in the feed because 
-            # we get the old cached version.
-            cached2 = make_page()
-            assert work2.title not in cached2.content
-            assert cached2.timestamp == old_timestamp
+        # The new work does not show up in the feed because 
+        # we get the old cached version.
+        cached2 = make_page()
+        assert work2.title not in cached2.content
+        assert cached2.timestamp == old_timestamp
             
-            # Change the policy to disable caching, and we get
-            # a brand new page with the new work.
-            config['policies'][Configuration.PAGE_MAX_AGE_POLICY] = 0
+        # Change the policy to disable caching, and we get
+        # a brand new page with the new work.
+        policy.value = "0"
 
-            cached3 = make_page()
-            assert cached3.timestamp > old_timestamp
-            assert work2.title in cached3.content
+        cached3 = make_page()
+        assert cached3.timestamp > old_timestamp
+        assert work2.title in cached3.content
 
 
 class TestAcquisitionFeed(DatabaseTest):
