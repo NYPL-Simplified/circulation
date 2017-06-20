ebfeb42c01407ecf375b78c78ba69455b6089eda
Amy Slagle <amyslagle@nypl.org>
2017-06-20 14:47:30 -0400

    Use the library with the lowest id for the admin interface for now.

--- api/admin/routes.py
+++ api/admin/routes.py
@@ -388,7 +388,7 @@ def admin_view(collection=None, book=None, **kwargs):
             return redirect(app.manager.url_for('admin_sign_in', redirect=redirect_url))
         # TODO: Design the admin interface for multiple libraries.
         # Until then, pick some library so the admin interface can run.
-        library = app.manager._db.query(Library).all()[0]
+        library = app.manager._db.query(Library).order_by(Library.id).all()[0]
         home_url = app.manager.url_for('acquisition_groups', library_short_name=library.short_name)
     else:
         home_url = None
