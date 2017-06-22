9aa5370d5c00ea474f106c374c6cd68cf0404582
Leonard Richardson <leonardr@segfault.org>
2017-06-22 14:26:27 -0400

    Simplify and fix the test of auto-reload when a library's name changes.

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -622,33 +622,35 @@ class TestBaseController(CirculationControllerTest):
             )
 
     def test_library_for_request_reloads_settings_if_necessary(self):
-
+        
         # We're about to change the shortname of the default library.
         new_name = "newname" + self._str
 
         # Before we make the change, a request to the library's new name
         # will fail.
         assert new_name not in self.manager.auth.library_authenticators
-        with self.app.test_request_context("/"):
-            problem = self.controller.library_for_request(new_name)
-            eq_(LIBRARY_NOT_FOUND, problem)
+        with self.default_config():
+            with self.app.test_request_context("/"):
+                problem = self.controller.library_for_request(new_name)
+                eq_(LIBRARY_NOT_FOUND, problem)
 
-        # Make the change, and the configuration settings are
-        # automatically reloaded to take the library's new name into
-        # account.
+        # Make the change.
         self._default_library.short_name = new_name
         self._db.commit()
 
-        # Now we can use the same Controller and the same
-        # CirculationManager to call the library by its new name.
-        assert new_name in self.manager.auth.library_authenticators
-        with self.app.test_request_context("/"):
-            value = self.controller.library_for_request(
-                self._default_library.short_name
-            )
-            eq_(self._default_library, value)
-            eq_(self._default_library, flask.request.library)
+        # Just making the change was not enough to update the
+        # CirculationManager's settings.
+        assert new_name not in self.manager.auth.library_authenticators
         
+        # But the first time we make a request that calls the library
+        # by its new name, those settings are reloaded.
+        with self.default_config():
+            with self.app.test_request_context("/"):
+                value = self.controller.library_for_request(new_name)
+
+                # An assertion that would have failed before works now.
+                assert new_name in self.manager.auth.library_authenticators
+
 
 class FullLaneSetupTest(CirculationControllerTest):
     """Most lane-based tests don't need the full multi-tier setup of lanes
