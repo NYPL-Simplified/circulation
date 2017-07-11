9209f1d8be435567558c532cd40314373655ce71
Dan Strauss <dans@datalogics.com>
2017-07-11 10:44:01 -0500

    Name parsing better handles corner cases now

--- api/millenium_patron.py
+++ api/millenium_patron.py
@@ -174,7 +174,8 @@ class MilleniumPatronAPI(BasicAuthenticationProvider, XMLParser):
         if actual_name.find(',') != -1:
             actual_family_name = actual_name.split(',')[0]
         else:
-            actual_family_name = actual_name.split(' ')[1]
+            actual_name_split = actual_name.split(' ')
+            actual_family_name = actual_name_split[len(actual_name_split)-1]
         if actual_family_name.upper() == supposed_family_name.upper():
             return True
         return False
