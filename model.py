76c29940e69c5ae51233d9ba954d50b10eb0c823
Leonard Richardson <leonardr@segfault.org>
2017-08-16 12:24:58 -0400

    Forgot to handle the case where new_value is None.

--- model.py
+++ model.py
@@ -9697,7 +9697,9 @@ class ConfigurationSetting(Base, HasFullTableCache):
     
     @value.setter
     def set_value(self, new_value):
-        self._value = unicode(new_value)
+        if new_value:
+            new_value = unicode(new_value)
+        self._value = new_value
 
     @classmethod
     def _is_secret(self, key):
