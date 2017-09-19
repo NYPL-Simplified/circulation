ddd407c38c0448806f4cf0048607b429fc92a6a7
Leonard Richardson <leonardr@segfault.org>
2017-09-19 13:45:45 -0400

    Allow the local analytics provider to work with or without an association with a specific library.

--- tests/test_local_analytics_provider.py
+++ tests/test_local_analytics_provider.py
@@ -13,11 +13,13 @@ import datetime
 class TestLocalAnalyticsProvider(DatabaseTest):
 
     def test_collect_event(self):
+        library2 = self._library()
+
         integration, ignore = create(
             self._db, ExternalIntegration,
             goal=ExternalIntegration.ANALYTICS_GOAL,
             protocol="core.local_analytics_provider")
-        la = LocalAnalyticsProvider(integration)
+        la = LocalAnalyticsProvider(integration, self._default_library)
         work = self._work(
             title="title", authors="author", fiction=True,
             audience="audience", language="lang",
@@ -28,11 +30,34 @@ class TestLocalAnalyticsProvider(DatabaseTest):
         la.collect_event(
             self._default_library, lp, CirculationEvent.DISTRIBUTOR_CHECKIN, now,
             old_value=None, new_value=None)
-        [event] = self._db \
-            .query(CirculationEvent) \
-            .filter(CirculationEvent.type == CirculationEvent.DISTRIBUTOR_CHECKIN) \
-            .all()
+
+        qu = self._db.query(CirculationEvent).filter(
+            CirculationEvent.type == CirculationEvent.DISTRIBUTOR_CHECKIN
+        )
+        eq_(1, qu.count())
+        [event] = qu.all()
 
         eq_(lp, event.license_pool)
         eq_(CirculationEvent.DISTRIBUTOR_CHECKIN, event.type)
         eq_(now, event.start)
+
+        # The LocalAnalyticsProvider will not handle an event intended
+        # for a different library.
+        now = datetime.datetime.now()
+        la.collect_event(
+            library2, lp, CirculationEvent.DISTRIBUTOR_CHECKIN, now,
+            old_value=None, new_value=None)
+        eq_(1, qu.count())
+
+        # It's possible to instantiate the LocalAnalyticsProvider
+        # without a library.
+        la = LocalAnalyticsProvider(integration)
+
+        # In that case, it will process events for any library.
+        for library in [self._default_library, library2]:
+            now = datetime.datetime.now()
+            la.collect_event(library, lp, 
+                             CirculationEvent.DISTRIBUTOR_CHECKIN, now,
+                             old_value=None, new_value=None
+            )
+        eq_(3, qu.count())
