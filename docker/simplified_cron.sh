76ca76da088bc78b667dc9a91c037dde6db6c769
Courteney Ervin <courteneyervin@nypl.org>
2017-09-25 07:12:05 -0400

    Change crontab name from libsimple to simplified

--- docker/simplified_cron.sh
+++ docker/simplified_cron.sh
@@ -4,5 +4,5 @@
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
 
 # Create cron tasks & logfile
-cp /ls_build/services/libsimple_crontab /etc/cron.d/circulation
+cp /ls_build/services/simplified_crontab /etc/cron.d/circulation
 touch /var/log/cron.log
