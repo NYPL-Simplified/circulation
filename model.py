4330e27d4e424c9cf9c12afecbcb79710aa43730
Leonard Richardson <leonardr@segfault.org>
2017-11-20 15:58:08 -0500

    Add test of CoverageRecord.lookup() with collection.

--- model.py
+++ model.py
@@ -1370,7 +1370,7 @@ class CoverageRecord(Base, BaseCoverageRecord):
         )
 
     @classmethod
-    def lookup(self, edition_or_identifier, data_source, operation=None,
+    def lookup(cls, edition_or_identifier, data_source, operation=None,
                collection=None):
         _db = Session.object_session(edition_or_identifier)
         if isinstance(edition_or_identifier, Identifier):
