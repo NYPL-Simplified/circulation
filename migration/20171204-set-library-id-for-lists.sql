f0c8d5ea17e57754c114afd72ac2e58126401703
Leonard Richardson <leonardr@segfault.org>
2017-12-04 15:31:45 -0500

    Only Staff Picks lists need this treatment -- it's OK for NYT lists not to be associated with any library.

--- migration/20171204-set-library-id-for-lists.sql
+++ migration/20171204-set-library-id-for-lists.sql
@@ -1,5 +1,5 @@
--- Associate all customlists with the default library, unless they're
--- already associated with some other library.
+-- Associate all 'Staff Picks' customlists with the default library,
+-- unless they're already associated with some other library.
 update customlists set library_id = (
  select id from libraries where is_default=true limit 1
-) where library_id is null;
+) where library_id is null and foreign_identifier='Staff Picks';
