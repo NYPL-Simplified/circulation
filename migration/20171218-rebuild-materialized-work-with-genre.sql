7a62c46b7571b1ebfab2a4fc37becf9cc73a8527
Leonard Richardson <leonardr@segfault.org>
2017-12-15 10:13:58 -0500

    Change lanes.inherit_parent_restrictions to default to true.

--- migration/20171218-rebuild-materialized-work-with-genre.sql
+++ migration/20171218-rebuild-materialized-work-with-genre.sql
@@ -1,3 +1,10 @@
+-- The default for lanes.inherit_parent_restrictions has changed
+-- to true. Change the setting for all lanes not based on custom lists.
+-- For all lanes based on custom lists, a human has made a decision
+-- to set the value one way or the other.
+update lanes set inherit_parent_restrictions=true where _list_datasource_id is null and id not in (select l.id from lanes l join lanes_customlists);
+ALTER TABLE lanes ALTER COLUMN inherit_parent_restrictions SET DEFAULT true;
+
 drop materialized view mv_works_editions_workgenres_datasources_identifiers;
 create materialized view mv_works_editions_workgenres_datasources_identifiers
 as
