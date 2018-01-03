a67c1d4d897bfd98f1e2c697218dc6f11e880eb2
Leonard Richardson <leonardr@segfault.org>
2018-01-03 13:33:37 -0500

    Added temporary logging.

--- api/opds.py
+++ api/opds.py
@@ -252,6 +252,8 @@ class CirculationManagerAnnotator(Annotator):
         return url
 
     def annotate_work_entry(self, work, active_license_pool, edition, identifier, feed, entry):
+        import time
+        a = time.time()
         Annotator.annotate_work_entry(
             work, active_license_pool, edition, identifier, feed, entry
         )
@@ -344,6 +346,8 @@ class CirculationManagerAnnotator(Annotator):
                 _external=True
             )
         )
+        b = time.time()
+        logging.error("Made entry for %s in %.2f" % (work.sort_title, b-a))
 
     @classmethod
     def related_books_available(cls, work, library):
