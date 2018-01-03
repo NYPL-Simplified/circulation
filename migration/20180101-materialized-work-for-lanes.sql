07378f0ab6c07169ebc40373e9e60b8328771173
Leonard Richardson <leonardr@segfault.org>
2018-01-03 12:58:48 -0500

    Migration script should create the new materialized view, not recreate one of the old ones in a way we don't want.

--- migration/20180101-materialized-work-for-lanes.sql
+++ migration/20180101-materialized-work-for-lanes.sql
@@ -5,8 +5,7 @@
 update lanes set inherit_parent_restrictions=true where _list_datasource_id is null and id not in (select lane_id from lanes_customlists);
 ALTER TABLE lanes ALTER COLUMN inherit_parent_restrictions SET DEFAULT true;
 
-drop materialized view mv_works_editions_workgenres_datasources_identifiers;
-create materialized view mv_works_editions_workgenres_datasources_identifiers
+create materialized view mv_works_for_lanes
 as
  SELECT 
     works.id AS works_id,
@@ -55,15 +54,25 @@ as
   ORDER BY (editions.sort_title, editions.sort_author, licensepools.availability_time);
 
 -- Create a work/genre lookup.
-create unique index mv_works_genres_work_id_genre_id on mv_works_editions_workgenres_datasources_identifiers (works_id, genre_id);
+create unique index mv_works_for_lanes_work_id_genre_id on mv_works_for_lanes (works_id, genre_id);
 
 -- Create an index on everything, sorted by descending availability time, so that sync feeds are fast.
 
-create index mv_works_genres_by_availability on mv_works_editions_workgenres_datasources_identifiers (availability_time DESC, sort_author, sort_title, works_id);
+create index mv_works_for_lanes_by_availability on mv_works_for_lanes (availability_time DESC, sort_author, sort_title, works_id);
+
+-- Create indexes that are helpful in running the query to find featured works.
+
+create index mv_works_for_lanes_by_random_and_genre on mv_works_for_lanes (random, language, genre_id);
+
+-- Not sure we need these two.
+
+-- create index mv_works_for_lanes_by_random_and_audience on mv_works_for_lanes (random, language, audience, target_age);
+
+-- create index mv_works_for_lanes_by_random_and_fiction on mv_works_for_lanes (random, language, fiction);
 
 -- Similarly, an index on everything, sorted by descending update time.
 
-create index mv_works_genres_by_modification on mv_works_editions_workgenres_datasources_identifiers (last_update_time DESC, sort_author, sort_title, works_id);
+create index mv_works_for_lanes_by_modification on mv_works_for_lanes (last_update_time DESC, sort_author, sort_title, works_id);
 
 -- We need three versions of each index:
 --- One that orders by sort_author, sort_title, and works_id
@@ -72,50 +81,50 @@ create index mv_works_genres_by_modification on mv_works_editions_workgenres_dat
 
 -- English adult fiction
 
-create index mv_works_genres_english_adult_fiction_by_author on mv_works_editions_workgenres_datasources_identifiers (sort_author, sort_title, works_id) WHERE audience in ('Adult', 'Adults Only') AND fiction = true AND language = 'eng';
+create index mv_works_for_lanes_english_adult_fiction_by_author on mv_works_for_lanes (sort_author, sort_title, works_id) WHERE audience in ('Adult', 'Adults Only') AND fiction = true AND language = 'eng';
 
-create index mv_works_genres_english_adult_fiction_by_title on mv_works_editions_workgenres_datasources_identifiers (sort_title, sort_author, works_id) WHERE audience in ('Adult', 'Adults Only') AND fiction = true AND language = 'eng';
+create index mv_works_for_lanes_english_adult_fiction_by_title on mv_works_for_lanes (sort_title, sort_author, works_id) WHERE audience in ('Adult', 'Adults Only') AND fiction = true AND language = 'eng';
 
-create index mv_works_genres_english_adult_fiction_by_availability on mv_works_editions_workgenres_datasources_identifiers (availability_time DESC, sort_author, sort_title, works_id) WHERE audience in ('Adult', 'Adults Only') AND fiction = true AND language = 'eng';
+create index mv_works_for_lanes_english_adult_fiction_by_availability on mv_works_for_lanes (availability_time DESC, sort_author, sort_title, works_id) WHERE audience in ('Adult', 'Adults Only') AND fiction = true AND language = 'eng';
 
 -- English adult nonfiction
 
-create index mv_works_genres_english_adult_nonfiction_by_author on mv_works_editions_workgenres_datasources_identifiers (sort_author, sort_title, works_id) WHERE audience in ('Adult', 'Adults Only') AND fiction = false AND language = 'eng';
+create index mv_works_for_lanes_english_adult_nonfiction_by_author on mv_works_for_lanes (sort_author, sort_title, works_id) WHERE audience in ('Adult', 'Adults Only') AND fiction = false AND language = 'eng';
 
-create index mv_works_genres_english_adult_nonfiction_by_title on mv_works_editions_workgenres_datasources_identifiers (sort_title, sort_author, works_id) WHERE audience in ('Adult', 'Adults Only') AND fiction = false AND language = 'eng';
+create index mv_works_for_lanes_english_adult_nonfiction_by_title on mv_works_for_lanes (sort_title, sort_author, works_id) WHERE audience in ('Adult', 'Adults Only') AND fiction = false AND language = 'eng';
 
-create index mv_works_genres_english_adult_nonfiction_by_availability on mv_works_editions_workgenres_datasources_identifiers (availability_time DESC, sort_author, sort_title, works_id) WHERE audience in ('Adult', 'Adults Only') AND fiction = false AND language = 'eng';
+create index mv_works_for_lanes_english_adult_nonfiction_by_availability on mv_works_for_lanes (availability_time DESC, sort_author, sort_title, works_id) WHERE audience in ('Adult', 'Adults Only') AND fiction = false AND language = 'eng';
 
 -- Nonenglish adult fiction
 --- These are also ordered by language
 
-create index mv_works_genres_nonenglish_adult_fiction_by_author on mv_works_editions_workgenres_datasources_identifiers (sort_author, sort_title, works_id, language) WHERE audience in ('Adult', 'Adults Only') AND fiction = true AND language <> 'eng';
+create index mv_works_for_lanes_nonenglish_adult_fiction_by_author on mv_works_for_lanes (sort_author, sort_title, works_id, language) WHERE audience in ('Adult', 'Adults Only') AND fiction = true AND language <> 'eng';
 
-create index mv_works_genres_nonenglish_adult_fiction_by_title on mv_works_editions_workgenres_datasources_identifiers (sort_title, sort_author, works_id, language) WHERE audience in ('Adult', 'Adults Only') AND fiction = true AND language <> 'eng';
+create index mv_works_for_lanes_nonenglish_adult_fiction_by_title on mv_works_for_lanes (sort_title, sort_author, works_id, language) WHERE audience in ('Adult', 'Adults Only') AND fiction = true AND language <> 'eng';
 
-create index mv_works_genres_nonenglish_adult_fiction_by_availability on mv_works_editions_workgenres_datasources_identifiers (availability_time DESC, sort_author, sort_title, works_id, language) WHERE audience in ('Adult', 'Adults Only') AND fiction = true AND language <> 'eng';
+create index mv_works_for_lanes_nonenglish_adult_fiction_by_availability on mv_works_for_lanes (availability_time DESC, sort_author, sort_title, works_id, language) WHERE audience in ('Adult', 'Adults Only') AND fiction = true AND language <> 'eng';
 
 -- Nonenglish adult nonfiction
 --- These are also ordered by language
 
-create index mv_works_genres_nonenglish_adult_nonfiction_by_author on mv_works_editions_workgenres_datasources_identifiers (sort_author, sort_title, works_id, language) WHERE audience in ('Adult', 'Adults Only') AND fiction = false AND language <> 'eng';
+create index mv_works_for_lanes_nonenglish_adult_nonfiction_by_author on mv_works_for_lanes (sort_author, sort_title, works_id, language) WHERE audience in ('Adult', 'Adults Only') AND fiction = false AND language <> 'eng';
 
-create index mv_works_genres_nonenglish_adult_nonfiction_by_title on mv_works_editions_workgenres_datasources_identifiers (sort_title, sort_author, works_id, language) WHERE audience in ('Adult', 'Adults Only') AND fiction = false AND language <> 'eng';
+create index mv_works_for_lanes_nonenglish_adult_nonfiction_by_title on mv_works_for_lanes (sort_title, sort_author, works_id, language) WHERE audience in ('Adult', 'Adults Only') AND fiction = false AND language <> 'eng';
 
-create index mv_works_genres_nonenglish_adult_nonfiction_by_availability on mv_works_editions_workgenres_datasources_identifiers (availability_time DESC, sort_author, sort_title, works_id, language) WHERE audience in ('Adult', 'Adults Only') AND fiction = false AND language <> 'eng';
+create index mv_works_for_lanes_nonenglish_adult_nonfiction_by_availability on mv_works_for_lanes (availability_time DESC, sort_author, sort_title, works_id, language) WHERE audience in ('Adult', 'Adults Only') AND fiction = false AND language <> 'eng';
 
 -- YA/Children's fiction, regardless of language
 
-create index mv_works_genres_ya_fiction_by_author on mv_works_editions_workgenres_datasources_identifiers (sort_author, sort_title, language, works_id) WHERE audience in ('Children', 'Young Adult') AND fiction = true;
+create index mv_works_for_lanes_ya_fiction_by_author on mv_works_for_lanes (sort_author, sort_title, language, works_id) WHERE audience in ('Children', 'Young Adult') AND fiction = true;
 
-create index mv_works_genres_ya_fiction_by_title on mv_works_editions_workgenres_datasources_identifiers (sort_title, sort_author, language, works_id) WHERE audience in ('Children', 'Young Adult') AND fiction = true;
+create index mv_works_for_lanes_ya_fiction_by_title on mv_works_for_lanes (sort_title, sort_author, language, works_id) WHERE audience in ('Children', 'Young Adult') AND fiction = true;
 
-create index mv_works_genres_ya_fiction_by_availability on mv_works_editions_workgenres_datasources_identifiers (availability_time DESC, sort_author, sort_title, language, works_id) WHERE audience in ('Children', 'Young Adult') AND fiction = true;
+create index mv_works_for_lanes_ya_fiction_by_availability on mv_works_for_lanes (availability_time DESC, sort_author, sort_title, language, works_id) WHERE audience in ('Children', 'Young Adult') AND fiction = true;
 
 -- YA/Children's nonfiction, regardless of language
 
-create index mv_works_genres_ya_nonfiction_by_author on mv_works_editions_workgenres_datasources_identifiers (sort_author, sort_title, language, works_id) WHERE audience in ('Children', 'Young Adult') AND fiction = false;
+create index mv_works_for_lanes_ya_nonfiction_by_author on mv_works_for_lanes (sort_author, sort_title, language, works_id) WHERE audience in ('Children', 'Young Adult') AND fiction = false;
 
-create index mv_works_genres_ya_nonfiction_by_title on mv_works_editions_workgenres_datasources_identifiers (sort_title, sort_author, language, works_id) WHERE audience in ('Children', 'Young Adult') AND fiction = false;
+create index mv_works_for_lanes_ya_nonfiction_by_title on mv_works_for_lanes (sort_title, sort_author, language, works_id) WHERE audience in ('Children', 'Young Adult') AND fiction = false;
 
-create index mv_works_genres_ya_nonfiction_by_availability on mv_works_editions_workgenres_datasources_identifiers (availability_time DESC, sort_author, sort_title, language, works_id) WHERE audience in ('Children', 'Young Adult') AND fiction = false;
+create index mv_works_for_lanes_ya_nonfiction_by_availability on mv_works_for_lanes (availability_time DESC, sort_author, sort_title, language, works_id) WHERE audience in ('Children', 'Young Adult') AND fiction = false;
