c0393ef27404a3b48de44d94df091f9a0924b6a2
Leonard Richardson <leonardr@segfault.org>
2018-01-17 12:29:55 -0500

    mv_works_editions_work_id also needs to have a unique index.

--- migration/20180117-add-unique-index-to-materialized-view.sql
+++ migration/20180117-add-unique-index-to-materialized-view.sql
@@ -1,2 +1,8 @@
 drop index if exists mv_works_for_lanes_work_id_genre_id;
 create index mv_works_for_lanes_unique on mv_works_for_lanes (works_id, genre_id, license_pool_id);
+
+-- This materialized view will be deleted soon, but until it is deleted,
+-- it needs to have a unique index, and work+genre ID isn't necessarily
+-- unique.
+drop index if exists mv_works_editions_work_id;
+create unique index mv_works_editions_work_id on mv_works_editions_datasources_identifiers (works_id, license_pool_id);
