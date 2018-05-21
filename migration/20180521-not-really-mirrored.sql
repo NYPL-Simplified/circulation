7af5b0d0a77099b8f435b360cf0ff98dd97b4996
Leonard Richardson <leonardr@segfault.org>
2018-05-21 13:20:03 -0400

    Migrate representations that had set_as_mirrored() called on them.

--- migration/20180521-not-really-mirrored.sql
+++ migration/20180521-not-really-mirrored.sql
@@ -5,3 +5,8 @@
 update representations set url=mirror_url where url is null and mirror_url is not null;
 
 update representations set mirror_url=null where url is not null and mirrored_at is null and mirror_url is not null;
+
+-- A representation is never 'mirrored' to its original URL.
+-- More likely Representation.set_as_mirrored() was called, but we now
+-- prefer to leave mirror_url alone.
+update representations set mirror_url=null, mirrored_at=null where url=mirror_url;
