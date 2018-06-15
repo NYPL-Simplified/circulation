6ea5b4c45c9e8042291fe521513327ad441ea8de
Leonard Richardson <leonardr@segfault.org>
2018-06-15 13:54:47 -0400

    Create a basic self-test framework.

--- config.py
+++ config.py
@@ -14,8 +14,37 @@ from entrypoint import EntryPoint
 
 from sqlalchemy.exc import ArgumentError
 
-class CannotLoadConfiguration(Exception):
+
+class IntegrationException(Exception):
+    """There was a problem in the setup or usage of an external integration.
+    """
+    def __init__(self, message, diagnostic=None):
+        """Constructor.
+
+        :param message: The normal message passed to any Exception
+        constructor.
+
+        :param diagnostic: An extra human-readable explanation of the
+        problem.  This may include instructions on what bits of the
+        integration configuration might need to be changed.
+
+        For example, an API key might be wrong, or the API key might
+        be correct but the API provider might not have granted that
+        key enough permissions.
+        """
+        super(IntegrationSetupException, self).__init__(message)
+        self.diagnostic = diagnostic
+
+
+class CannotLoadConfiguration(IntegrationException):
+    """The current configuration of an external integration, or of the
+    site as a whole, is in an incomplete or inconsistent state.
+
+    This is more specific than an IntegrationException because it assumes the
+    problem is evident just by looking at the current configuration.
+    """
     pass
+
 
 @contextlib.contextmanager
 def temp_config(new_config=None, replacement_classes=None):
---
+++ external_integration.py
@@ -0,0 +1,83 @@
+"""A module to support the classes that implement the code
+configured by various ExternalIntegrations.
+"""
+from nose.tools import set_trace
+from config import IntegrationException
+import datetime
+
+
+class SelfTestResult(object):
+
+    def __init__(self, name):
+        # Name of the test.
+        self.name = name
+
+        # The test ran without raising an exception.
+        self.success = False
+
+        # The exception raised, if any.
+        self.exception = None
+
+        # The return value of the test method, assuming it ran to
+        # completion.
+        self.result = result
+
+        # Start time of the test.
+        self.start = datetime.datetime.utcnow()
+
+        # End time of the test.
+        self.end = None
+
+    def __repr__(self):
+        if self.exception:
+            if (isinstance(self.exception, IntegrationSetupException)
+                and self.exception.diagnostic):
+                exception = " exception=%r diagnostic=%r" % (
+                    self.exception.message, self.exception.diagnostic
+                )
+            else:
+                exception = " exception=%r" % self.exception
+        else:
+            exception = ""
+        return "<SelfTestResult: name=%r timing=%.2fsec success=%r%s>" % (
+            self.name, (self.end-self.start).total_seconds(), self.success,
+            exception
+        )
+
+
+class HasSelfTest(object):
+    """An object capable of verifying its own configuration by running a
+    self-test.
+    """
+
+    def self_test(self):
+        """Verify that this integration is properly configured and working.
+
+        :return: A list of SelfTestResult objects.
+        """
+        raise NotImplementedError()
+
+    def run_test(self, name, method, *args, **kwargs):
+        """Run a test method, record any exception that happens, and keep
+        track of how long the test takes to run.
+
+        :param name: The name of the test to be run.
+        :param method: A method to call to run the test.
+        :param args: Position arguments to `method`.
+        :param kwargs: Keyword arguments to `method`.
+
+        :return: A filled-in SelfTestResult.
+        """
+        result = SelfTestResult(name)
+        try:
+            result = method(*args, **kwargs)
+            result.success = True
+            result.result = result
+        except Exception, e:
+            result.exception = e
+            result.success = False
+            result.result = None
+        finally:
+            if not result.end:
+                result.end = datetime.datetime.utcnow()
+        return result
