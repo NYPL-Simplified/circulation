37102c3feae67a9feea23b9799ea3c7d0ee6a854
Leonard Richardson <leonardr@segfault.org>
2018-08-28 13:40:44 -0400

    The problem was with the collection IDs, not the target age filter.

--- external_search.py
+++ external_search.py
@@ -1141,7 +1141,6 @@ class Filter(SearchBase):
             ]
         }
 
-
         def does_not_exist(field):
             """A filter that matches if there is no value for `field`."""
             return F('bool', must_not=[F('exists', field=field)])
@@ -1152,9 +1151,9 @@ class Filter(SearchBase):
         upper_does_not_exist = does_not_exist("target_age.upper")
         upper_in_range = self._match_op("target_age.upper", "gte", lower)
 
-        lower_match = {"or": [lower_does_not_exist, lower_in_range]}
-        upper_match = {"or": [upper_does_not_exist, upper_in_range]}
-        final = {'and': [upper_match, lower_match]}
+        lower_match = F("or", [lower_does_not_exist, lower_in_range])
+        upper_match = F("or", [upper_does_not_exist, upper_in_range])
+        final = F('and', [upper_match, lower_match])
         return final
 
     @classmethod
