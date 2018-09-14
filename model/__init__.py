1fb3a2cf69d66234843dded218b83eb3f59cc77c
Adriana Alter <adrianaalter@nypl.org>
2018-09-14 09:34:10 -0400

    SessionManager: BaseMaterializedWork, SessionManager, MaterializedWorkWithGenre

--- model/__init__.py
+++ model/__init__.py
@@ -143,6 +143,8 @@ from sqlalchemy.dialects.postgresql import (
     INT4RANGE,
 )
 
+Base = declarative_base()
+
 from media_type_constants import MediaTypes
 from datasource_constants import DataSourceConstants
 from link_relations import LinkRelations
@@ -156,7 +158,10 @@ from helper_methods import (
     numericrange_to_tuple,
     tuple_to_numericrange,
 )
-
+from session_manager import (
+    BaseMaterializedWork,
+    SessionManager,
+)
 DEBUG = False
 
 def production_session():
@@ -176,11 +181,6 @@ def production_session():
     from log import LogConfiguration
     LogConfiguration.initialize(_db)
     return _db
-
-class BaseMaterializedWork(object):
-    """A mixin class for materialized views that incorporate Work and Edition."""
-    pass
-
 
 class HasFullTableCache(object):
     """A mixin class for ORM classes that maintain an in-memory cache of
@@ -314,204 +314,8 @@ class HasFullTableCache(object):
             _db, cls._cache, '_cache', cache_key, lookup_hook
         )
 
-class SessionManager(object):
 
-    # Materialized views need to be created and indexed from SQL
-    # commands kept in files. This dictionary maps the views to the
-    # SQL files.
 
-    MATERIALIZED_VIEW_WORKS = 'mv_works_editions_datasources_identifiers'
-    MATERIALIZED_VIEW_WORKS_WORKGENRES = 'mv_works_editions_workgenres_datasources_identifiers'
-    MATERIALIZED_VIEW_LANES = 'mv_works_for_lanes'
-    MATERIALIZED_VIEWS = {
-        #MATERIALIZED_VIEW_WORKS : 'materialized_view_works.sql',
-        #MATERIALIZED_VIEW_WORKS_WORKGENRES : 'materialized_view_works_workgenres.sql',
-        MATERIALIZED_VIEW_LANES : 'materialized_view_for_lanes.sql',
-    }
-
-    # A function that calculates recursively equivalent identifiers
-    # is also defined in SQL.
-    RECURSIVE_EQUIVALENTS_FUNCTION = 'recursive_equivalents.sql'
-
-    engine_for_url = {}
-
-    @classmethod
-    def engine(cls, url=None):
-        url = url or Configuration.database_url()
-        return create_engine(url, echo=DEBUG)
-
-    @classmethod
-    def sessionmaker(cls, url=None, session=None):
-        if not (url or session):
-            url = Configuration.database_url()
-        if url:
-            bind_obj = cls.engine(url)
-        elif session:
-            bind_obj = session.get_bind()
-            if not os.environ.get('TESTING'):
-                # If a factory is being created from a session in test mode,
-                # use the same Connection for all of the tests so objects can
-                # be accessed. Otherwise, bind against an Engine object.
-                bind_obj = bind_obj.engine
-        return sessionmaker(bind=bind_obj)
-
-    @classmethod
-    def initialize(cls, url, create_materialized_work_class=True):
-        if url in cls.engine_for_url:
-            engine = cls.engine_for_url[url]
-            return engine, engine.connect()
-
-        engine = cls.engine(url)
-        Base.metadata.create_all(engine)
-
-        base_path = os.path.split(__file__)[0]
-        resource_path = os.path.join(base_path, "files")
-
-        connection = None
-        for view_name, filename in cls.MATERIALIZED_VIEWS.items():
-            if engine.has_table(view_name):
-                continue
-            if not connection:
-                connection = engine.connect()
-            resource_file = os.path.join(resource_path, filename)
-            if not os.path.exists(resource_file):
-                raise IOError("Could not load materialized view from %s: file does not exist." % resource_file)
-            logging.info(
-                "Loading materialized view %s from %s.",
-                view_name, resource_file)
-            sql = open(resource_file).read()
-            connection.execution_options(isolation_level='AUTOCOMMIT')\
-                .execute(text(sql))
-
-            # NOTE: This is apparently necessary for the creation of
-            # the materialized view to be finalized in all cases. As
-            # such, materialized views should be created WITH NO DATA,
-            # since they will be refreshed immediately after creation.
-            result = connection.execute(
-                "REFRESH MATERIALIZED VIEW %s;" % view_name
-            )
-
-        if not connection:
-            connection = engine.connect()
-
-        # Check if the recursive equivalents function exists already.
-        query = select(
-            [literal_column('proname')]
-        ).select_from(
-            table('pg_proc')
-        ).where(
-            literal_column('proname')=='fn_recursive_equivalents'
-        )
-        result = connection.execute(query)
-        result = list(result)
-
-        # If it doesn't, create it.
-        if not result:
-            resource_file = os.path.join(resource_path, cls.RECURSIVE_EQUIVALENTS_FUNCTION)
-            if not os.path.exists(resource_file):
-                raise IOError("Could not load recursive equivalents function from %s: file does not exist." % resource_file)
-            sql = open(resource_file).read()
-            connection.execute(sql)
-
-        if connection:
-            connection.close()
-
-        if create_materialized_work_class:
-            class MaterializedWorkWithGenre(Base, BaseMaterializedWork):
-                __table__ = Table(
-                    cls.MATERIALIZED_VIEW_LANES,
-                    Base.metadata,
-                    Column('works_id', Integer, primary_key=True, index=True),
-                    Column('workgenres_id', Integer, primary_key=True, index=True),
-                    Column('list_id', Integer, ForeignKey('customlists.id'),
-                           primary_key=True, index=True),
-                    Column(
-                        'list_edition_id', Integer, ForeignKey('editions.id'),
-                        primary_key=True, index=True
-                    ),
-                    Column(
-                        'license_pool_id', Integer,
-                        ForeignKey('licensepools.id'), primary_key=True,
-                        index=True
-                    ),
-                    autoload=True,
-                    autoload_with=engine
-                )
-                license_pool = relationship(
-                    LicensePool,
-                    primaryjoin="LicensePool.id==MaterializedWorkWithGenre.license_pool_id",
-                    foreign_keys=LicensePool.id, lazy='joined', uselist=False)
-
-            globals()['MaterializedWorkWithGenre'] = MaterializedWorkWithGenre
-
-        cls.engine_for_url[url] = engine
-        return engine, engine.connect()
-
-    @classmethod
-    def refresh_materialized_views(self, _db):
-        for view_name in self.MATERIALIZED_VIEWS.keys():
-            _db.execute("refresh materialized view %s;" % view_name)
-            _db.commit()
-        # Immediately update the number of works associated with each
-        # lane.
-        from lane import Lane
-        for lane in _db.query(Lane):
-            lane.update_size(_db)
-
-    @classmethod
-    def session(cls, url, initialize_data=True):
-        engine = connection = 0
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore", category=sa_exc.SAWarning)
-            engine, connection = cls.initialize(
-                url, create_materialized_work_class=initialize_data
-            )
-        session = Session(connection)
-        if initialize_data:
-            session = cls.initialize_data(session)
-        return session
-
-    @classmethod
-    def initialize_data(cls, session, set_site_configuration=True):
-        # Create initial data sources.
-        list(DataSource.well_known_sources(session))
-
-        # Load all existing Genre objects.
-        Genre.populate_cache(session)
-
-        # Create any genres not in the database.
-        for g in classifier.genres.values():
-            # TODO: On the very first startup this is rather expensive
-            # because the cache is invalidated every time a Genre is
-            # created, then populated the next time a Genre is looked
-            # up. This wouldn't be a big problem, but this also happens
-            # on setup for the unit tests.
-            Genre.lookup(session, g, autocreate=True)
-
-        # Make sure that the mechanisms fulfillable by the default
-        # client are marked as such.
-        for content_type, drm_scheme in DeliveryMechanism.default_client_can_fulfill_lookup:
-            mechanism, is_new = DeliveryMechanism.lookup(
-                session, content_type, drm_scheme
-            )
-            mechanism.default_client_can_fulfill = True
-
-        # If there is currently no 'site configuration change'
-        # Timestamp in the database, create one.
-        timestamp, is_new = get_one_or_create(
-            session, Timestamp, collection=None,
-            service=Configuration.SITE_CONFIGURATION_CHANGED,
-            create_method_kwargs=dict(timestamp=datetime.datetime.utcnow())
-        )
-        if is_new:
-            site_configuration_has_changed(session)
-        session.commit()
-
-        # Return a potentially-new Session object in case
-        # it was updated by cls.update_timestamps_table
-        return session
-
-Base = declarative_base()
 
 class PresentationCalculationPolicy(object):
     """Which parts of the Work or Edition's presentation
