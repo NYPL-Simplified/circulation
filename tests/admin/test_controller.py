169c10be46b803cd1cc7daf738f5b70ac01eebff
Edwin Guzman <edwin.gzmn@gmail.com>
2018-09-19 15:00:15 -0400

    Updating unit tests for custom_list method.

--- tests/admin/test_controller.py
+++ tests/admin/test_controller.py
@@ -2174,37 +2174,28 @@ class TestCustomListsController(AdminControllerTest):
     def test_custom_list_get(self):
         data_source = DataSource.lookup(self._db, DataSource.LIBRARY_STAFF)
         list, ignore = create(self._db, CustomList, name=self._str, library=self._default_library, data_source=data_source)
-        edition = self._edition()
 
-        [c1] = edition.author_contributors
-        c1.display_name = self._str
-        c2, ignore = self._contributor()
-        c2.display_name = self._str
-        edition.add_contributor(c2, Contributor.AUTHOR_ROLE)
-        list.add_entry(edition)
-        work = self._work(with_license_pool=True)
-        list.add_entry(work)
-        collection = self._collection()
-        collection.customlists = [list]
+        work1 = self._work(with_license_pool=True)
+        work2 = self._work(with_license_pool=True)
+        list.add_entry(work1)
+        list.add_entry(work2)
+
         with self.request_context_with_library_and_admin("/"):
             response = self.manager.admin_custom_lists_controller.custom_list(list.id)
             feed = feedparser.parse(response.get_data())
-            testlist = list
 
             eq_(list.name, feed.feed.title)
-            # no entries, collections, entry_count
-            eq_(1, len(feed.entries))
-            # eq_(1, len(response.get("entries")))
-            [entry] = feed.entries
+            eq_(2, len(feed.entries))
+            [crawlable_link] = [x['href'] for x in feed.feed['links']
+                              if x['rel'] == "http://opds-spec.org/crawlable"]
+            eq_(crawlable_link, "http://localhost/default/lists/%s/crawlable" % list.name)
 
-            # eq_(work.id, entry.get("id"))
-            eq_(work.title, entry.get("title"))
-            # should be 2
-            eq_(1, len(entry.get("authors")))
-            # eq_(Edition.medium_to_additional_type[Edition.BOOK_MEDIUM], entry.get("medium"))
-            # eq_(work.language, entry.get("language"))
-            # eq_(set([c1.display_name, c2.display_name]),
-            #     set(entry.get("authors")))
+            [entry1, entry2] = feed.entries
+            eq_(work1.title, entry1.get("title"))
+            eq_(work2.title, entry2.get("title"))
+
+            eq_(work1.presentation_edition.author, entry1.get("author"))
+            eq_(work2.presentation_edition.author, entry2.get("author"))
 
     def test_custom_list_get_errors(self):
         with self.request_context_with_library_and_admin("/"):
