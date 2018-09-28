388d032251a5d736f3f6629be4afbb8db96af51e
Leonard Richardson <leonardr@segfault.org>
2018-09-28 10:58:20 -0400

    Test that facets are propagated through the search link.

--- scripts.py
+++ scripts.py
@@ -343,7 +343,7 @@ class CacheRepresentationPerLane(LaneSweeperScript):
         :param _db: A database connection.
         :param cmd_args: A mock set of command-line arguments, to use instead
            of looking at the actual command line.
-        :param testing: If this method creates a CirculationManager object, 
+        :param testing: If this method creates a CirculationManager object,
            this value will be passed in to its constructor as its value for
            `testing`.
         :param manager: A mock CirculationManager object, to use instead
@@ -352,7 +352,7 @@ class CacheRepresentationPerLane(LaneSweeperScript):
         :param *args: Positional arguments to pass to the superconstructor.
         :param **kwargs: Keyword arguments to pass to the superconstructor.
         """
-        
+
         super(CacheRepresentationPerLane, self).__init__(_db, *args, **kwargs)
         self.parse_args(cmd_args)
         if not manager:
