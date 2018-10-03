74e7011f5cd21064ed0d61cd599b1dc34abcf1b6
Leonard Richardson <leonardr@segfault.org>
2018-10-03 14:33:34 -0400

    Corrected migration script.

--- migration/20181002-deliverymechanism-media-type-can-be-null.sql
+++ migration/20181002-deliverymechanism-media-type-can-be-null.sql
@@ -5,4 +5,4 @@ ALTER TABLE deliverymechanisms ALTER COLUMN content_type DROP NOT NULL;
 -- MP3 files are not a content type for purposes of describing
 -- fulfillment, because they are always obtained by processing a
 -- manifest. In that case the manifest document is the content type.
-update deliverymechanisms set content_type=NULL where content_type='audio/mpeg' and content_type='application/vnd.librarysimplified.findaway.license+json';
+update deliverymechanisms set content_type=NULL where content_type='audio/mpeg' and drm_scheme='application/vnd.librarysimplified.findaway.license+json';
