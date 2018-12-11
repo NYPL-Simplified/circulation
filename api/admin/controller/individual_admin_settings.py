cb4aab12b83d61286a94ad6718447815a0f427e6
Adriana Alter <adrianaalter@nypl.org>
2018-12-11 11:12:03 -0500

    fix test

--- api/admin/controller/individual_admin_settings.py
+++ api/admin/controller/individual_admin_settings.py
@@ -231,6 +231,5 @@ class IndividualAdminSettingsController(SettingsController):
             self.require_system_admin()
         if not admin:
             return MISSING_ADMIN
-        # self._db.delete(admin)
-        # return Response(unicode(_("Deleted")), 200)
-        return MISSING_ADMIN
+        self._db.delete(admin)
+        return Response(unicode(_("Deleted")), 200)
