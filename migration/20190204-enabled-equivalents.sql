8a985a91324205a37899b17c5adf642dc48bd380
Edwin Guzman <edwin.gzmn@gmail.com>
2019-02-05 15:11:13 -0500

    Small change to latest migration script and checking for initialize_data in SessionManager initilization method.

--- migration/20190204-enabled-equivalents.sql
+++ migration/20190204-enabled-equivalents.sql
@@ -1,4 +1,4 @@
-alter table equivalents create column enabled default true;
+alter table equivalents add column enabled boolean default true;
 CREATE INDEX ix_equivalents_enabled ON equivalents USING btree (enabled);
 
 -- Delete the recursive_equivalents function -- it will be recreated in
