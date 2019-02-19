cd79106633d1c4137ea25dd85d53f6515b4c6562
Leonard Richardson <leonardr@segfault.org>
2019-02-19 15:18:21 -0500

    if not exists doesn't work here.

--- migration/20190219-add-licensepooldeliveries-constraint-if-not-exists.sql
+++ migration/20190219-add-licensepooldeliveries-constraint-if-not-exists.sql
@@ -1,7 +1,7 @@
 -- Make sure the unique constraint on licensepooldeliveries exists; it may not exist in older databases.
 DO $$
   BEGIN
-    alter table licensepooldeliveries add constraint if not exists licensepooldeliveries_data_source_id_identifier_id_delivery_key unique (data_source_id, identifier_id, delivery_mechanism_id, resource_id);
+    alter table licensepooldeliveries add constraint licensepooldeliveries_data_source_id_identifier_id_delivery_key unique (data_source_id, identifier_id, delivery_mechanism_id, resource_id);
   EXCEPTION
     WHEN duplicate_object THEN RAISE NOTICE 'service_type already exists, not creating it.';
   END;
