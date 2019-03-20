aca15690cd4446256966763f939677bb411839cd
Amy Slagle <amslagle@gmail.com>
2019-03-20 14:20:15 -0400

    Updated DPLA exchange ODL feed URL and removed outdated configuration settings.

--- migration/20190320-2-remove-odl-consolidated-licenses.sql
+++ migration/20190320-2-remove-odl-consolidated-licenses.sql
@@ -6,3 +6,12 @@ update licensepools set licenses_available = 0 from collections join externalint
 delete from coveragerecords where coveragerecords.identifier_id in (select identifiers.id from identifiers join licensepools on licensepools.identifier_id = identifiers.id join collections on licensepools.collection_id = collections.id join externalintegrations on collections.external_integration_id = externalintegrations.id where externalintegrations.protocol = 'ODL With Consolidated Copies') and coveragerecords.operation = 'import';
 
 update externalintegrations set protocol = 'ODL' where protocol = 'ODL with Consolidated Copies';
+
+-- Update the URL for any collections using the DPLA Exchange.
+
+update collections set external_account_id = 'https://www.feedbooks.com/harvest/odl' where external_account_id = 'https://www.feedbooks.com/library/last_update.atom';
+
+-- Delete configuration settings that are no longer needed.
+
+delete from configurationsettings where key = 'consolidated_copies_url';
+delete from configurationsettings where key = 'consolidated_loan_url';
