7b84ef3d16896fd5ada53799517ec64dcf4b7085
Edwin Guzman <edwin.gzmn@gmail.com>
2019-04-04 10:35:52 -0400

    Adding set_to_delete field in the Collections class.

--- migration/20190404-add-deletion-flag-collections.sql
+++ migration/20190404-add-deletion-flag-collections.sql
@@ -1,2 +1,2 @@
 -- Add a marked_for_deletion column to keep track if a collection should be deleted.
-alter table collections add COLUMN "marked_for_deletion" boolean default false;
+alter table collections add column "marked_for_deletion" boolean default false;
