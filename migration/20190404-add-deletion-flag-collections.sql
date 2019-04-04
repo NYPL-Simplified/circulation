449ce8a519aaba272f360e5d63dee789f7ede6ce
Edwin Guzman <edwin.gzmn@gmail.com>
2019-04-04 10:26:35 -0400

    Updating column name.

--- migration/20190404-add-deletion-flag-collections.sql
+++ migration/20190404-add-deletion-flag-collections.sql
@@ -1,2 +1,2 @@
--- Add a set_to_delete column to keep track if a collection should be deleted.
-alter table collections add COLUMN "set_to_delete" boolean default false;
+-- Add a marked_for_deletion column to keep track if a collection should be deleted.
+alter table collections add COLUMN "marked_for_deletion" boolean default false;
