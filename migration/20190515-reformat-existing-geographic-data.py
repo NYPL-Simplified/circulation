0eccb9f3d991aa793dd92e471da5270069a0e496
Adriana Alter <adrianaalter@nypl.org>
2019-05-16 11:34:20 -0400

    Process multiple formats

--- migration/20190515-reformat-existing-geographic-data.py
+++ migration/20190515-reformat-existing-geographic-data.py
@@ -1,12 +1,12 @@
 from nose.tools import set_trace
 from sqlalchemy.sql import *
+import ast
 import json
 import os
 import sys
 bin_dir = os.path.split(__file__)[0]
 package_dir = os.path.join(bin_dir, "..")
 sys.path.append(os.path.abspath(package_dir))
-from api.admin.geographic_validator import GeographicValidator
 from core.model import (
     Library,
     production_session
@@ -20,18 +20,37 @@ area_settings = _db.query(ConfigurationSetting).filter(
     or_(ConfigurationSetting._value != None, ConfigurationSetting._value == "")
     ).all()
 
-settings_with_wrong_format = []
-for s in area_settings:
+def convert_to_list(string):
+    # If the string is in the format '["Waterford, CT"]', load it as a list
+    # so that each item in it can be formatted
     try:
-        if not type(json.loads(s._value)) is dict:
-            settings_with_wrong_format.append(s)
+        return json.loads(ast.literal_eval(string))
     except:
-        settings_with_wrong_format.append(s)
+        return None
 
-for setting in settings_with_wrong_format:
-    formatted_info = GeographicValidator().validate_geographic_areas(setting._value, _db)
+def format(value):
+    result = []
+    try:
+        value = convert_to_list(value) or json.loads(value)
+        if type(value) is list:
+            for x in value:
+                result += format(x)
+        elif type(value) is not dict:
+            result += json.loads(value)
+    except:
+        result.append(value)
+
+    return result
+
+for setting in area_settings:
     library = _db.query(Library).filter(Library.id == setting.library_id).first()
-    ConfigurationSetting.for_library_and_externalintegration(_db, setting.key, library, None).value = formatted_info
+    result = format(setting._value)
+    formatted_info = None
+    if result:
+        formatted_info = json.dumps({"US": result})
+
+    if formatted_info:
+        ConfigurationSetting.for_library_and_externalintegration(_db, setting.key, library, None).value = formatted_info
 
 _db.commit()
 _db.close()
