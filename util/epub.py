ba54b6b625e5b843e9f9d990c670211a5b562feb
Edwin Guzman <edwin.gzmn@gmail.com>
2019-07-31 14:41:46 -0400

    Adding workout to get lxml.builder.ElementMaker to be pickled by Sphinx.

--- util/epub.py
+++ util/epub.py
@@ -23,7 +23,7 @@ class EpubAccessor(object):
             the absence of the `content` parameter
         :param content: A string representing the compressed EPUB
 
-        :return : A tuple containing a ZipFile of the EPUB and the path to its
+        :returns: A tuple containing a ZipFile of the EPUB and the path to its
             package
         """
         if not (url or content):
