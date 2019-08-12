bc060989e2457ea76a36ed23804d934f69ff6cd1
Leonard Richardson <leonardr@segfault.org>
2019-08-12 09:58:39 -0400

    Simplified and fixed test of Collection.metadata_identifier.

--- tests/models/test_collection.py
+++ tests/models/test_collection.py
@@ -409,14 +409,13 @@ class TestCollection(DatabaseTest):
         assert_raises(ValueError, getattr, self.collection, 'metadata_identifier')
 
         def build_expected(protocol, unique_id):
-            # util.string_helpers.base64 takes care of encoding and
-            # decoding the strings we're base64-encoding, so we don't
-            # need to worry about that. However, we do need to pass in
-            # a bytestring for 'altchars', an extra argument that's
-            # passed right along to the standard library's base64
-            # implementation.
-            encoded = [base64.b64encode(value, b'-_') for value in [protocol, unique_id]]
+            encode = base64.urlsafe_b64encode
+            encoded = [
+                encode(value)
+                for value in [protocol, unique_id]
+            ]
             joined = ':'.join(encoded)
+            return encode(joined)
 
         # With a unique identifier, we get back the expected identifier.
         self.collection.external_account_id = 'id'
