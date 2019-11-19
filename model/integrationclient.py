7bdd2874595ca8a1a4f9cb017fb92e562467f652
Leonard Richardson <leonardr@segfault.org>
2019-11-19 15:16:45 -0500

    Added IntegrationClient.enabled.

--- model/integrationclient.py
+++ model/integrationclient.py
@@ -41,6 +41,10 @@ class IntegrationClient(Base):
     # Shared secret
     shared_secret = Column(Unicode, unique=True, index=True)
 
+    # It may be necessary to disable an integration client until it
+    # upgrades to fix a known bug.
+    enabled = Column(Boolean, default=True)
+
     created = Column(DateTime)
     last_accessed = Column(DateTime)
 
