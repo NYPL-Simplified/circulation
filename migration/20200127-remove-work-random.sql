1e90155f0a64d2e254bddc0d60b813790259350a
Leonard Richardson <leonardr@segfault.org>
2020-01-28 11:32:27 -0500

    If a materialized view is sticking around for some reason, drop it when works.random is dropped.

--- migration/20200127-remove-work-random.sql
+++ migration/20200127-remove-work-random.sql
@@ -3,4 +3,4 @@ drop index if exists ix_works_audience_target_age_quality_random;
 drop index if exists ix_works_audience_fiction_quality_random;
 
 -- Remove the works.random column itself.
-ALTER TABLE works DROP COLUMN if exists random;
+ALTER TABLE works DROP COLUMN if exists random CASCADE;
