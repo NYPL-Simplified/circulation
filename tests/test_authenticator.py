2254949f7ebdb9d78dfd088376cee9d1779795b5
Kristo Jorgenson <kristo.jorgenson@gmail.com>
2020-07-01 14:08:59 +0200

    fix typo

--- tests/test_authenticator.py
+++ tests/test_authenticator.py
@@ -1276,7 +1276,7 @@ class TestLibraryAuthenticator(AuthenticatorTest):
 
             # The mobile color scheme and web colors are correctly reported.
             eq_("plaid", doc['color_scheme'])
-            eq_("#377F8B", doc['web_color_scheme']['primaru'])
+            eq_("#377F8B", doc['web_color_scheme']['primary'])
             eq_("#D53F34", doc['web_color_scheme']['secondary'])
 
             # _geographic_areas was called and provided the library's
