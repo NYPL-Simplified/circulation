ca220daa6df06146670e5fbac4f5ff22e274536c
Jonathan Green <jonathan@razzard.com>
2021-03-12 09:53:20 -0400

    Remove pycharm debugging code.

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,9 +9,3 @@ from os.path import abspath, dirname, basename
 # Pull in the session_fixture defined in core/testing.py
 # which does the database setup and initialization
 pytest_plugins = ["{}.testing".format(basename(dirname(dirname(abspath(__file__)))))]
-
-try:
-    import pydevd_pycharm
-    pydevd_pycharm.settrace('localhost', port=8567, stdoutToServer=True, stderrToServer=True)
-except Exception:
-    pass
