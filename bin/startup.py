db34fc56ce54f24ad5141534d664250cd21c687f
Jonathan Green <jonathan@razzard.com>
2021-03-16 16:58:27 -0300

    Fix import path issue
    We were using nose to change the import path for the files imported
    in the core/bin folder. Directly manipulate the import path in order to
    remove this dependancy on nose.

--- bin/startup.py
+++ bin/startup.py
@@ -4,7 +4,7 @@ from os import sys, path
 # https://stackoverflow.com/questions/11536764/how-to-fix-attempted-relative-import-in-non-package-even-with-init-py
 # Once we have a stable package name for core, it should be easier to do away with something like this
 # for now we add the core component path to the sys.path when we are running these scripts
-component_dir = path.dirname(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
+component_dir = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
 
 # Load the 'core' module as though this script were being run from
 # the parent component (either circulation or metadata).
