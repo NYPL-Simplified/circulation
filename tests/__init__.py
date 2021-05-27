394fa7c6c262fb9425d47d5435b406dcdf962de0
Leonard Richardson <leonardr@segfault.org>
2021-05-27 11:56:55 -0400

    All sample data should come in as bytestrings by default.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 
-def sample_data(filename, sample_data_dir, mode="r"):
+def sample_data(filename, sample_data_dir, mode="rb"):
     base_path = os.path.split(__file__)[0]
     resource_path = os.path.join(base_path, "files", sample_data_dir)
     path = os.path.join(resource_path, filename)
