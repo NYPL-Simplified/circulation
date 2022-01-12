15cad239c2b3a2ad0d4c157e14ff563519e5e8de
Keith Bauer <bauer.keith@gmail.com>
2022-01-12 09:46:10 -0600

    Check if authentication_flow_document is a list or a single entry before checking its 'type'

--- util/authentication_for_opds.py
+++ util/authentication_for_opds.py
@@ -12,13 +12,21 @@ class OPDSAuthenticationFlow(object):
         `authentication` list of an Authentication For OPDS document.
         """
         data = self._authentication_flow_document(_db)
+        if isinstance(data, list):
+            for entry in data:
+                self._get_type(entry)
+        else:
+            self._get_type(data)
+
+        return data
+
+    def _get_type(self, data):
         if not data.get('type'):
             data['type'] = self.FLOW_TYPE
         if not data.get('type'):
             raise ValueError(
                 "Authentication flow document for %r does not include required field 'type'" % self
             )
-        return data
 
     def _authentication_flow_document(self, _db):
         raise NotImplementedError()
