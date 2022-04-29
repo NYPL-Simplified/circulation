eaba2609e21286d6b6bb68e1ef29c49acf0804bc
Keith Bauer <bauer.keith@gmail.com>
2022-04-29 09:57:32 -0500

    Remove unique constraints

--- core/model/patron.py
+++ core/model/patron.py
@@ -511,13 +511,13 @@ class Loan(Base, LoanAndHoldMixin):
     # Some distributors (e.g. Feedbooks) may have an identifier that can
     # be used to check the status of a specific Loan.
     external_identifier = Column(Unicode, unique=True, nullable=True)
-    cached_manifest = Column(Unicode, unique=True, nullable=True)
-    cached_content_type = Column(Unicode, unique=True, nullable=True)
+    cached_manifest = Column(Unicode, nullable=True)
+    cached_content_type = Column(Unicode, nullable=True)
 
     __table_args__ = (
         UniqueConstraint('patron_id', 'license_pool_id'),
     )
-    
+
     def __lt__(self, other):
         return self.id < other.id
 
