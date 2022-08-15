1cf83db09db6d3f4c6c6f20fc2bbd65b8354e302
Bob Scales <thinkwebengineering@gmail.com>
2022-08-15 16:06:40 +0200

    Add documentation for admin/analitics_service/service_id

--- api/admin/routes.py
+++ api/admin/routes.py
@@ -2261,6 +2261,52 @@ def analytics_services():
 @requires_admin
 @requires_csrf_token
 def analytics_service(service_id):
+    """Delete an analytics service.
+    ---
+    delete:
+      tags:
+        - analytics
+      summary: Delete an analytics service.
+      security:
+        - BasicAuth: []
+      parameters:
+        - X-CSRF-Token
+        - in: path
+          name: service_id
+          description: Id of analytics service to be deleted.
+          schema:
+            type: string
+      responses:
+        200:
+          description: Analytics service has been deleted.
+          content:
+            text/html:
+              schema: |
+                type: string
+        4XX:
+          description: |
+            These are anticipated errors due to a malformed request, invalid option, 
+            or other issue with the current request. These are returned as JSON objects with 
+            a uniquely identifying URI. Possible URIs for this endpoint are:
+            * `http://librarysimplified.org/terms/problem/incomplete-configuration`
+            * `http://librarysimplified.org/terms/problem/invalid-email`
+            * `http://librarysimplified.org/terms/problem/unknown-role`
+            * `http://librarysimplified.org/terms/problem/library-not-found`
+            * `http://librarysimplified.org/terms/problem/missing-pgcrypto-extension`
+          content:
+            application/json:
+              schema: ProblemResponse
+        5XX:
+          description: |
+            An unanticipated bug in the system that could not be properly handled.
+
+            If the API server is running in debug mode the output will contain a traceback, 
+            otherwise a basic error message will be displayed.
+          content:
+            text/html:
+              example: An internal error occurred
+              schema: ProblemResponse
+    """
     return app.manager.admin_analytics_services_controller.process_delete(service_id)
 
 
